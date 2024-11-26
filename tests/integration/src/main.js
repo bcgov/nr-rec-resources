076530852fd5827aeba411091bceb667291c0d78
marcellmueller <marcel@button.is>
2024-11-26 14:17:01 -0800

    chore: move prettier config up one directory

--- tests/integration/src/main.js
+++ tests/integration/src/main.js
@@ -19,7 +19,13 @@ const BASE_URL = process.env.BASE_URL;
 
 async function performEachMethod(BASE_URL, testCase, method, id) {
   let url = BASE_URL + testCase.path;
-  if (id && (method === "GET" || method === "PUT" || method === "PATCH" || method === "DELETE")) {
+  if (
+    id &&
+    (method === "GET" ||
+      method === "PUT" ||
+      method === "PATCH" ||
+      method === "DELETE")
+  ) {
     if (url.endsWith("/") === false) {
       url = url + "/" + id;
     } else {
@@ -38,19 +44,26 @@ async function performEachMethod(BASE_URL, testCase, method, id) {
     method: method,
     url: url,
     headers: {
-      ...testCase.headers
+      ...testCase.headers,
     },
-    data: payload
+    data: payload,
   });
   console.info(`Response for ${method} ${url} : ${response.status}`);
-  const methodAssertion = testCase.assertions.find(assertion => assertion.method === method);
+  const methodAssertion = testCase.assertions.find(
+    (assertion) => assertion.method === method,
+  );
   const responseData = response.data?.data || response.data;
   if (methodAssertion) {
     if (methodAssertion.status_code) {
       assert(response.status === methodAssertion.status_code);
     }
     if (methodAssertion.body) {
-      assert(isEqual(omit(responseData, testCase.data.id_field), methodAssertion.body) === true);
+      assert(
+        isEqual(
+          omit(responseData, testCase.data.id_field),
+          methodAssertion.body,
+        ) === true,
+      );
     }
   }
   if (method === "POST") {
@@ -61,11 +74,18 @@ async function performEachMethod(BASE_URL, testCase, method, id) {
 async function performTesting(testSuitesDir, testSuiteFile) {
   console.info(`Running test suite for : ${testSuiteFile}`);
   const testSuitePath = path.join(testSuitesDir, testSuiteFile);
-  const testSuite = JSON.parse(await fs.promises.readFile(testSuitePath, "utf-8"));
+  const testSuite = JSON.parse(
+    await fs.promises.readFile(testSuitePath, "utf-8"),
+  );
   for (const testCase of testSuite.tests) {
     let id = null;
     for (const method of testCase.methods) {
-      const responseId = await performEachMethod(BASE_URL, testCase, method, id);
+      const responseId = await performEachMethod(
+        BASE_URL,
+        testCase,
+        method,
+        id,
+      );
       if (responseId) {
         id = responseId;
       }
@@ -76,7 +96,7 @@ async function performTesting(testSuitesDir, testSuiteFile) {
 const main = async () => {
   const testSuitesDir = path.join(__dirname, "test_suites");
   const testSuiteFiles = await fs.promises.readdir(testSuitesDir);
-  const testFile = testSuiteFiles.find(file => file.includes(apiName));
+  const testFile = testSuiteFiles.find((file) => file.includes(apiName));
   await performTesting(testSuitesDir, testFile);
 };
 
@@ -90,5 +110,3 @@ try {
   console.error(e);
   process.exit(137);
 }
-
-
