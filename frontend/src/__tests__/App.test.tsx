329633d5822384dd016e3c49d15c4fb239b4422f
marcellmueller <marcel@button.is>
2024-12-02 14:47:36 -0800

    chore: fix frontend test

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -4,6 +4,8 @@ import { render, screen } from '../test-utils';
 describe('Simple working test', () => {
   it('the title is visible', () => {
     render(<App />);
-    expect(screen.getByText(/QuickStart OpenShift/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/Recreation Sites and Trails BC/i),
+    ).toBeInTheDocument();
   });
 });
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,31 +3,14 @@ import '@digitalspace/bcparks-bootstrap-theme/dist/css/bootstrap-theme.min.css';
 import '@bcgov/bc-sans/css/BC_Sans.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppRoutes from '@/routes';
-import Footer from '@/components/layout/Footer';
-import Header from '@/components/layout/Header';
-import { BrowserRouter } from 'react-router-dom';
+import App from '@/App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
-    <Header />
-    <main
-      // use bcparks bootstrap theme container class to share same page max width
-      className="container"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        flex: 1,
-        flexGrow: 1,
-        minHeight: '100%',
-      }}
-    >
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </main>
-    <Footer />
+    <App />
   </StrictMode>,
 );
+
+export default root;
