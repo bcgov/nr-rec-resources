d3509e0dda019f384dce5a9c9472a1802aa5fc26
Marcel Mueller <marcel@button.is>
2024-12-02 15:35:42 -0800

    Revert "chore: move prettier config up one directory"
    This reverts commit e101251f89d475a31550c4925b3bf73d0d545976.

--- backend/src/app.spec.ts
+++ backend/src/app.spec.ts
@@ -1,16 +1,19 @@
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { bootstrap } from "./app";
-jest.mock("prom-client", () => ({
-  Registry: jest.fn().mockImplementation(() => ({})),
-  collectDefaultMetrics: jest.fn().mockImplementation(() => ({})),
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {bootstrap} from "./app";
+jest.mock('prom-client', () => ({
+  Registry: jest.fn().mockImplementation(() => ({
+  })),
+  collectDefaultMetrics: jest.fn().mockImplementation(() => ({
+  })),
 }));
-jest.mock("express-prom-bundle", () => ({
-  default: jest.fn().mockImplementation(() => ({})),
+jest.mock('express-prom-bundle', () => ({
+  default: jest.fn().mockImplementation(() => ({
+  })),
 }));
-jest.mock("./prom", () => ({
+jest.mock('./prom', () => ({
   metricsMiddleware: jest.fn().mockImplementation((req, res, next) => next()),
 }));
-describe("main", () => {
+describe('main', () => {
   let app: NestExpressApplication;
 
   beforeAll(async () => {
@@ -21,7 +24,7 @@ describe("main", () => {
     await app.close();
   });
 
-  it("should start the application", async () => {
+  it('should start the application', async () => {
     expect(app).toBeDefined();
   });
 });
