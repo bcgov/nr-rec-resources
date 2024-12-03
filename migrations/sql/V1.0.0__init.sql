2ba7c084f09fdb673df0383fcaf8a818d905eb1f
marcellmueller <marcel@button.is>
2024-12-03 08:49:25 -0800

    chore: add recreation_resource table

--- migrations/sql/V1.0.0__init.sql
+++ migrations/sql/V1.0.0__init.sql
@@ -9,9 +9,9 @@ CREATE SEQUENCE IF NOT EXISTS USERS."USER_SEQ"
 
 CREATE TABLE IF NOT EXISTS USERS.USERS
 (
-    ID    numeric      not null
+    ID numeric not null
         constraint "USER_PK"
-            primary key DEFAULT nextval('USERS."USER_SEQ"'),
+        primary key DEFAULT nextval('USERS."USER_SEQ"'),
     NAME  varchar(200) not null,
     EMAIL varchar(200) not null
 );
@@ -21,3 +21,21 @@ VALUES ('John', 'John.ipsum@test.com'),
        ('Jack', 'Jack.ipsum@test.com'),
        ('Jill', 'Jill.ipsum@test.com'),
        ('Joe', 'Joe.ipsum@test.com');
+
+
+
+CREATE SCHEMA IF NOT EXISTS RST;
+
+CREATE TABLE IF NOT EXISTS RST.RECREATION_RESOURCE
+(
+    FOREST_FILE_ID varchar(200) not null primary key,
+    NAME           varchar(200) not null,
+    DESCRIPTION    varchar(200) not null
+);
+
+INSERT INTO RST.RECREATION_RESOURCE (FOREST_FILE_ID, NAME, DESCRIPTION)
+VALUES ('1', 'Rec site 1', 'Rec site 1 description'),
+       ('2', 'Rec site 2', 'Rec site 2 description'),
+       ('3', 'Rec site 3', 'Rec site 3 description'),
+       ('4', 'Rec site 4', 'Rec site 4 description'),
+       ('5', 'Rec site 5', 'Rec site 5 description');
