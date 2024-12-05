e04ee69d07be34f433d0af8999f86e1bd2d2fc9f
marcellmueller <marcel@button.is>
2024-12-05 08:53:19 -0800

    test: fix app.tsx test

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,9 +1,16 @@
 import App from '@/App';
-import { render, screen } from '../test-utils';
+import { act, render, screen } from '@testing-library/react';
+import apiService from '@/service/api-service';
+
+vi.spyOn(apiService as any, 'getAxiosInstance').mockReturnValue({
+  get: vi.fn().mockResolvedValue({}),
+});
 
 describe('Simple working test', () => {
-  it('the title is visible', () => {
-    render(<App />);
+  it('the title is visible', async () => {
+    await act(async () => {
+      render(<App />);
+    });
     expect(
       screen.getByText(/Recreation Sites and Trails BC/i),
     ).toBeInTheDocument();
