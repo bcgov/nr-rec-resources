5b77aa2123b13803850d2b1768aee6243a635c08
marcellmueller <marcel@button.is>
2024-12-06 12:21:23 -0800

    chore: switch from cloudformation templates to cdk

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as path from "path";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const s3CorsRule: s3.CorsRule = {
+      allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.HEAD],
+      allowedOrigins: ["*"],
+      allowedHeaders: ["*"],
+      maxAge: 300,
+    };
+
+    const s3Bucket = new s3.Bucket(this, "S3Bucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      cors: [s3CorsRule],
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      objectLockEnabled: false,
+    });
+
+    const oai = new cloudfront.OriginAccessIdentity(this, "OAI");
+    s3Bucket.grantRead(oai);
+
+    new s3deploy.BucketDeployment(this, "DeployFrontend", {
+      sources: [
+        s3deploy.Source.asset(path.join(__dirname, "../../frontend/dist")),
+      ],
+      destinationBucket: s3Bucket,
+      distribution: new cloudfront.Distribution(this, "FrontendCF", {
+        defaultBehavior: {
+          origin: new origins.S3Origin(s3Bucket, { originAccessIdentity: oai }),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      }),
+    });
+  }
+}
