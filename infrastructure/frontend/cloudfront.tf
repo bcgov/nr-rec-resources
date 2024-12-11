6022e6e98ad6ea6f4c6c8982ff2be1bc141e7586
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2024-12-11 10:49:26 -0800

    fix: trying to fix routing for SPA

--- infrastructure/frontend/cloudfront.tf
+++ infrastructure/frontend/cloudfront.tf
@@ -1,4 +1,22 @@
 data "aws_caller_identity" "current" {}
+locals {
+  src_dir = "./"
+  content_type_map = {
+    html = "text/html",
+    ico  = "image/x-icon",
+    js   = "application/javascript",
+    json = "application/json",
+    svg  = "image/svg+xml",
+    ttf  = "font/ttf",
+    txt  = "text/txt",
+    css  = "text/css"
+  }
+  files_raw = fileset(local.src_dir, "**")
+  files = toset([
+    for jsFile in local.files_raw:
+      jsFile if jsFile != ".terragrunt-source-manifest" && jsFile != "assets/.terragrunt-source-manifest"
+  ])
+}
 
 resource "aws_s3_bucket" "frontend" {
   bucket = "${var.app_name}-frontend-${data.aws_caller_identity.current.account_id}-${var.aws_region}"
@@ -54,6 +72,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
   }
+  # Required for SPA application to redirect requests to deep links to root, which loads index.html, which loads the Vue app
+  # and then the Vue router properly resolves the deep link.
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/"
+  }
+
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
@@ -87,4 +113,17 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   tags = {
     Name = "${var.app_name}-distribution"
   }
+}
+
+resource "aws_s3_object" "site_files" {
+  
+  for_each = local.files
+
+  # Create an object from each
+  bucket = aws_s3_bucket.frontend.id
+  key    = each.value
+  source = "${local.src_dir}/${each.value}"
+  etag = filemd5("${local.src_dir}/${each.value}")
+
+  content_type = lookup(local.content_type_map, regex("\\.(?P<extension>[A-Za-z0-9]+)$", each.value).extension, "application/octet-stream")
 }
