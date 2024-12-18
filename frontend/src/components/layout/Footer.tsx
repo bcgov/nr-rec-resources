bdecde6555e7d024086c466bb80c18642217e914
marcellmueller <marcel@button.is>
2024-12-18 08:00:32 -0800

    chore: update header and footer styling

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,62 +1,83 @@
+import RSTLogo from '@/images/RST_nav_logo.svg';
+import '@/styles/Footer.scss';
+
 const Footer = () => {
   return (
-    <footer className="footer">
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="container">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www2.gov.bc.ca/gov/content?id=79F93E018712422FBC8E674A67A70535"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Disclaimer
+    <footer id="footer">
+      <div className="home-footer" id="home-footer">
+        <div className="row no-gutters">
+          <div className="col col-12 col-md-4">
+            <div className="mb-5">
+              <a className="d-inline-block" href="/">
+                <img
+                  src={RSTLogo}
+                  alt="Recreation Sites and Trails BC Logo"
+                  style={{ height: 64 }}
+                />
               </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www2.gov.bc.ca/gov/content?id=9E890E16955E4FF4BF3B0E07B4722932"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Privacy
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www2.gov.bc.ca/gov/content?id=E08E79740F9C41B9B0C484685CC5E412"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Accessibility
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www2.gov.bc.ca/gov/content?id=1AAACC9C65754E4D89A118B875E0FBDA"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Copyright
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="https://www2.gov.bc.ca/gov/content?id=6A77C17D0CCB48F897F8598CCC019111"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Contact Us
-              </a>
-            </li>
-          </ul>
+            </div>
+          </div>
+          <div className="col col-12 col-md-8">
+            {/* <div className="row no-gutters"> */}
+            {/*   {footerMenu.map((item, index) => ( */}
+            {/*     <FooterMenu item={item} key={index} /> */}
+            {/*   ))} */}
+            {/* </div> */}
+          </div>
         </div>
-      </nav>
+        <div className="text-left text-sm-center pt-4 mt-5 border-top border-white">
+          <div className="footer-utility-link d-inline-block">
+            <a
+              className="nav-link"
+              href="https://www2.gov.bc.ca/gov/content?id=79F93E018712422FBC8E674A67A70535"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Disclaimer
+            </a>
+          </div>
+          <div className="footer-utility-link d-inline-block">
+            <a
+              className="nav-link"
+              href="https://www2.gov.bc.ca/gov/content?id=9E890E16955E4FF4BF3B0E07B4722932"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Privacy
+            </a>
+          </div>
+          <div className="footer-utility-link d-inline-block">
+            <a
+              className="nav-link"
+              href="https://www2.gov.bc.ca/gov/content?id=E08E79740F9C41B9B0C484685CC5E412"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Accessibility
+            </a>
+          </div>
+          <div className="footer-utility-link d-inline-block">
+            <a
+              className="nav-link"
+              href="https://www2.gov.bc.ca/gov/content?id=1AAACC9C65754E4D89A118B875E0FBDA"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Copyright
+            </a>
+          </div>
+          <div className="footer-utility-link d-inline-block">
+            <a
+              className="nav-link"
+              href="https://www2.gov.bc.ca/gov/content?id=6A77C17D0CCB48F897F8598CCC019111"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Contact Us
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
