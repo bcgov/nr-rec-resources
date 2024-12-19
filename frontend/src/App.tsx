70a49404d4e390b974e661a0ecf66f7b12b375a8
marcellmueller <marcel@button.is>
2024-12-18 20:43:25 -0800

    refactor: remove page max width on main element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main className="page-padding">
+      <main>
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
