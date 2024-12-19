a2de9cdb2bf4fc1d251057602e1880cce5539e40
marcellmueller <marcel@button.is>
2024-12-19 11:09:23 -0800

    chore: style contact table

--- frontend/src/components/RecResource/Contact.tsx
+++ frontend/src/components/RecResource/Contact.tsx
@@ -2,18 +2,29 @@ const Contact = () => {
   return (
     <section>
       <h2 className="section-heading">Contact</h2>
-      <table className="table">
-        <tbody>
-          <tr>
-            <th>Site Operator</th>
-            <td>Placeholder</td>
-          </tr>
-          <tr>
-            <th>Phone</th>
-            <td>Placeholder</td>
-          </tr>
-        </tbody>
-      </table>
+      <figure className="table">
+        <table>
+          <tbody>
+            <tr>
+              <th>Site Operator</th>
+              <td>
+                <p>Placeholder</p>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                General questions and feedback for Recreation Sites & Trails BC
+              </th>
+              <td>
+                <p>We answer emails weekdays from 9 am to 5 pm Pacific Time.</p>
+                <p>
+                  <a href="">Contact Us</a>
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </figure>
     </section>
   );
 };
