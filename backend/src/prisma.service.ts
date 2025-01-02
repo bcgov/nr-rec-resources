0e12322f4c5cb3193fec2f29b57e22403633007a
marcellmueller <marcel@button.is>
2025-01-02 14:25:16 -0800

    chore: add datasource url to prisma service

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,8 +1,28 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
+const DB_HOST = process.env.POSTGRES_HOST || "localhost";
+const DB_USER = process.env.POSTGRES_USER || "postgres";
+const DB_PWD = encodeURIComponent(process.env.POSTGRES_PASSWORD || "default"); // this needs to be encoded, if the password contains special characters it will break connection string.
+const DB_PORT = process.env.POSTGRES_PORT || 5432;
+const DB_NAME = process.env.POSTGRES_DATABASE || "postgres";
+const DB_SCHEMA = process.env.POSTGRES_SCHEMA || "rst";
+const dataSourceURL = `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`;
+
 @Injectable()
 class PrismaService extends PrismaClient implements OnModuleInit {
+  databaseUrl: string;
+
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: dataSourceURL,
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
