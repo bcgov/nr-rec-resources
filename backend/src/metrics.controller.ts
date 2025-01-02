b56055d351034a5576439082db63dead90239847
marcellmueller <marcel@button.is>
2025-01-02 15:42:52 -0800

    chore: move prom to middleware folder

--- backend/src/metrics.controller.ts
+++ backend/src/metrics.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Res } from "@nestjs/common";
 import { Response } from "express";
-import { register } from "./prom";
+import { register } from "src/middleware/prom";
 import { PrismaService } from "src/prisma.service";
 @Controller("metrics")
 export class MetricsController {
