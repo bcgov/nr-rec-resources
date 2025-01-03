84e6e0bf127e04a09c36232641f1d57d1123d9e7
marcellmueller <marcel@button.is>
2025-01-03 13:51:17 -0800

    chore: search multiple fields

--- backend/src/recreation-resource/recreation-resource.service.ts
+++ backend/src/recreation-resource/recreation-resource.service.ts
@@ -49,7 +49,14 @@ export class RecreationResourceService {
     const recreationResources = await this.prisma.recreation_resource.findMany({
       skip: (page - 1) * limit,
       take: parseInt(String(limit)),
-      where: filter ? { name: { contains: filter, mode: "insensitive" } } : {},
+      where: {
+        OR: [
+          { forest_file_id: { contains: filter, mode: "insensitive" } },
+          { name: { contains: filter, mode: "insensitive" } },
+          { description: { contains: filter, mode: "insensitive" } },
+          { site_location: { contains: filter, mode: "insensitive" } },
+        ],
+      },
     });
 
     const count = recreationResources.length;
@@ -61,35 +68,5 @@ export class RecreationResourceService {
       total: count,
       totalPages: Math.ceil(count / limit),
     };
-  }
-
-  public convertFiltersToPrismaFormat(filterObj): any {
-    const prismaFilterObj = {};
-
-    for (const item of filterObj) {
-      if (item.operation === "like") {
-        prismaFilterObj[item.key] = { contains: item.value };
-      } else if (item.operation === "eq") {
-        prismaFilterObj[item.key] = { equals: item.value };
-      } else if (item.operation === "neq") {
-        prismaFilterObj[item.key] = { not: { equals: item.value } };
-      } else if (item.operation === "gt") {
-        prismaFilterObj[item.key] = { gt: item.value };
-      } else if (item.operation === "gte") {
-        prismaFilterObj[item.key] = { gte: item.value };
-      } else if (item.operation === "lt") {
-        prismaFilterObj[item.key] = { lt: item.value };
-      } else if (item.operation === "lte") {
-        prismaFilterObj[item.key] = { lte: item.value };
-      } else if (item.operation === "in") {
-        prismaFilterObj[item.key] = { in: item.value };
-      } else if (item.operation === "notin") {
-        prismaFilterObj[item.key] = { not: { in: item.value } };
-      } else if (item.operation === "isnull") {
-        prismaFilterObj[item.key] = { equals: null };
-      }
-    }
-    console.log(prismaFilterObj);
-    return prismaFilterObj;
   }
 }
