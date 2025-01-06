3dcb23782337ce227f984bfb66fb1c0b5559e130
marcellmueller <marcel@button.is>
2025-01-06 08:37:03 -0800

    chore: set search params on input change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -79,6 +79,7 @@ export default [
         "error",
         {
           argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
         },
       ],
 
--- frontend/src/components/Search/SearchBanner.tsx
+++ frontend/src/components/Search/SearchBanner.tsx
@@ -1,11 +1,28 @@
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import debounce from 'lodash.debounce';
 import '@/styles/components/Search.scss';
 
 const SearchBanner = () => {
+  const [_, setSearchParams] = useSearchParams();
+
+  const onSearchChange = useMemo(
+    () =>
+      debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchParams({ filter: e.target.value });
+      }, 100),
+    [setSearchParams],
+  );
+
   return (
     <div className="page-nav-container search">
       <nav aria-label="Search banner" className="page-nav search-banner">
         <h1>Find a recreation site or trail</h1>
-        <input type="text" placeholder="Search..."></input>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={onSearchChange}
+        ></input>
       </nav>
     </div>
   );
