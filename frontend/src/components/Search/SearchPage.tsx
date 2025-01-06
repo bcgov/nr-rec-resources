3e5ecb2540662eb0b3062aaf0d639af1c1882ed3
marcellmueller <marcel@button.is>
2025-01-06 09:22:27 -0800

    chore: query api on search param change

--- frontend/src/components/Search/SearchPage.tsx
+++ frontend/src/components/Search/SearchPage.tsx
@@ -1,51 +1,88 @@
-import apiService from '@/service/api-service';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import apiService from '@/service/api-service';
+import type { AxiosResponse } from '~/axios';
 import RecResourceCard from '@/components/Search/RecResourceCard';
 import SearchBanner from '@/components/Search/SearchBanner';
-import type { AxiosResponse } from '~/axios';
 
 const SearchPage = () => {
   const [recResources, setRecResources] = useState<any>([]);
+  const [searchParams] = useSearchParams();
+
+  const filter = searchParams.get('filter');
 
   useEffect(() => {
-    // Get all recreation resources
-    apiService
-      .getAxiosInstance()
-      .get('/v1/recreation-resource')
-      .then((response: AxiosResponse) => {
-        const recreationResources = [];
-        for (const resource of response.data) {
-          const recreationResourceDto = {
-            forest_file_id: resource.forest_file_id,
-            name: resource.name,
-            description: resource.description,
-          };
-          recreationResources.push(recreationResourceDto);
-        }
-        setRecResources(recreationResources);
-        return recreationResources;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    if (!filter) {
+      // Get all recreation resources
+      apiService
+        .getAxiosInstance()
+        .get('/v1/recreation-resource')
+        .then((response: AxiosResponse) => {
+          const recreationResources = [];
+          console.log(response.data);
+          for (const resource of response.data) {
+            recreationResources.push(resource);
+          }
+          setRecResources(recreationResources);
+          return recreationResources;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    // eslint-disable-next-line
   }, []);
 
+  useEffect(() => {
+    if (filter) {
+      // Get recreation resources by filter
+      apiService
+        .getAxiosInstance()
+        .get(`/v1/recreation-resource/search?filter=${filter}&page=1&limit=10`)
+        .then((response: AxiosResponse) => {
+          const recreationResources = [];
+          const res = response.data;
+          for (const resource of res.data) {
+            const recreationResourceDto = {
+              forest_file_id: resource.forest_file_id,
+              name: resource.name,
+              description: resource.description,
+            };
+            recreationResources.push(recreationResourceDto);
+          }
+          setRecResources(recreationResources);
+          return recreationResources;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [filter]);
+
+  const isResults = recResources.length > 0;
   return (
     <>
       <SearchBanner />
       <div className="page page-padding">
         <section>
-          {recResources.map((resource: any) => {
-            const { forest_file_id, name, description } = resource;
-            return (
-              <RecResourceCard
-                key={forest_file_id}
-                forest_file_id={forest_file_id}
-                name={name}
-                description={description}
-              />
-            );
-          })}
+          {isResults ? (
+            <>
+              {recResources.map((resource: any) => {
+                const { forest_file_id, name, description } = resource;
+                return (
+                  <RecResourceCard
+                    key={forest_file_id}
+                    forest_file_id={forest_file_id}
+                    name={name}
+                    description={description}
+                  />
+                );
+              })}
+            </>
+          ) : (
+            <p>No results found.</p>
+          )}
         </section>
       </div>
     </>
