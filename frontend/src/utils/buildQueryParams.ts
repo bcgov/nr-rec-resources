3ab16c463b37295b719784e1736c10c09b4ee9f2
marcellmueller <marcel@button.is>
2025-01-10 10:11:03 -0800

    chore: add build query params util

---
+++ frontend/src/utils/buildQueryParams.ts
@@ -0,0 +1,16 @@
+// Function to build a query string from an object of key-value pairs
+// eg: buildQueryParams({ page: "1", search: "search" }) => "?page=1&search=search"
+
+const buildQueryParams = (params: {
+  [key: string]: string | number | undefined;
+}) => {
+  if (!params) return '';
+  const query = Object.entries(params)
+
+    .filter(([_, value]) => value !== '' && value !== undefined)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+  return `?${query}`;
+};
+
+export default buildQueryParams;
