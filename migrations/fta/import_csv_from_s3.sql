ba84069d93460ca941df75f72f12272d24b94306
marcellmueller <marcel@button.is>
2025-01-15 15:08:46 -0800

    chore: add recreation_risk_site s3 import

--- migrations/fta/import_csv_from_s3.sql
+++ migrations/fta/import_csv_from_s3.sql
@@ -340,3 +340,13 @@ select aws_s3.table_import_from_s3(
   )',
   aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_RISK_RATING_CODE.csv', 'ca-central-1')
 );
+
+select aws_s3.table_import_from_s3(
+  'fta.recreation_risk_site',
+  '',
+  '(
+    FORMAT csv,
+    HEADER true
+  )',
+  aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_RISK_SITE.csv', 'ca-central-1')
+);
