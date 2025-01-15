827a0434792c400ccd59daad68d51def1f17db8d
marcellmueller <marcel@button.is>
2025-01-15 15:14:30 -0800

    chore: add recreation_struct_dimen_xref s3 import

--- migrations/fta/import_csv_from_s3.sql
+++ migrations/fta/import_csv_from_s3.sql
@@ -391,3 +391,13 @@ select aws_s3.table_import_from_s3(
   )',
   aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_STRUCT_DIMEN_CODE.csv', 'ca-central-1')
 );
+
+select aws_s3.table_import_from_s3(
+  'fta.recreation_struct_dimen_xref',
+  '',
+  '(
+    FORMAT csv,
+    HEADER true
+  )',
+  aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_STRUCT_DIMEN_XREF.csv', 'ca-central-1')
+);
