fc49c1964f592a34e20fde43c9b84c95b6c55e0d
marcellmueller <marcel@button.is>
2025-01-15 15:24:45 -0800

    chore: add recreation_structure_value s3 import

--- migrations/fta/import_csv_from_s3.sql
+++ migrations/fta/import_csv_from_s3.sql
@@ -438,3 +438,13 @@ select aws_s3.table_import_from_s3(
   )',
   aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_STRUCTURE.csv', 'ca-central-1')
 );
+
+select aws_s3.table_import_from_s3(
+  'fta.recreation_structure_value',
+  '',
+  '(
+    FORMAT csv,
+    HEADER true
+  )',
+  aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_STRUCTURE_VALUE.csv', 'ca-central-1')
+);
