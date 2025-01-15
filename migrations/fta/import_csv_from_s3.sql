fc1c210a463f35b7d71c358f8895f9d85ec35c4b
marcellmueller <marcel@button.is>
2025-01-15 15:13:38 -0800

    chore: add recreation_struct_dimen_code s3 import

--- migrations/fta/import_csv_from_s3.sql
+++ migrations/fta/import_csv_from_s3.sql
@@ -381,3 +381,13 @@ select aws_s3.table_import_from_s3(
   )',
   aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_SITE_POINT.csv', 'ca-central-1')
 );
+
+select aws_s3.table_import_from_s3(
+  'fta.recreation_struct_dimen_code',
+  '',
+  '(
+    FORMAT csv,
+    HEADER true
+  )',
+  aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_STRUCT_DIMEN_CODE.csv', 'ca-central-1')
+);
