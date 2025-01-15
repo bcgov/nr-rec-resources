c47baa78a72b9de0c1de47b3bb8777ac7d189e36
marcellmueller <marcel@button.is>
2025-01-15 13:36:42 -0800

    chore: add recreation_file_status_code s3 import

--- migrations/fta/import_csv_from_s3.sql
+++ migrations/fta/import_csv_from_s3.sql
@@ -200,3 +200,13 @@ select aws_s3.table_import_from_s3(
   )',
   aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_FEE.csv', 'ca-central-1')
 );
+
+select aws_s3.table_import_from_s3(
+  'fta.recreation_file_status_code',
+  '',
+  '(
+    FORMAT csv,
+    HEADER true
+  )',
+  aws_commons.create_s3_uri('rst-fta-dataload-oracle', 'RECREATION_FILE_STATUS_CODE.csv', 'ca-central-1')
+);
