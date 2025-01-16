53352f69bd51dd7f6fda6e2ec14f723a984d58ea
marcellmueller <marcel@button.is>
2025-01-16 12:22:50 -0800

    chore: add fta_to_rst migration

--- migrations/rst/sql/V1.0.0__init.sql
+++ migrations/rst/sql/V1.0.0__init.sql
@@ -5,9 +5,9 @@ create schema if not exists rst;
 create table if not exists rst.recreation_resource
 (
     forest_file_id varchar(200) not null primary key,
-    name           varchar(200) not null,
-    description    varchar(5000) not null,
-    site_location  varchar(200) not null
+    name           varchar(200),
+    description    varchar(5000),
+    site_location  varchar(200)
 );
 
 comment on table rst.recreation_resource is 'Resource information relating to a recreational file. A recreation file can have only one resource. A recreation resource must be of type Site, Reserve, Trail, or Interpretive Forest.';
