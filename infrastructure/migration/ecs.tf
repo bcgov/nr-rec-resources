f78d7321b8cf372a24af9626b592bece6b56201b
marcellmueller <marcel@button.is>
2025-01-16 22:39:46 -0800

    chore: remove duplicate resources

--- infrastructure/migration/ecs.tf
+++ infrastructure/migration/ecs.tf
@@ -34,8 +34,8 @@ resource "aws_ecs_task_definition" "flyway_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.app_container_role.arn
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = data.aws_iam_role.app_container_role.arn
   container_definitions = jsonencode([
     {
       name      = "${var.app_name}-flyway"
