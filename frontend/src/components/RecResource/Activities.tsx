267f607c1d1f24b93dfe147fc03c24c5e49b740c
marcellmueller <marcel@button.is>
2025-01-21 10:57:02 -0800

    refactor: use rec activity database description

--- frontend/src/components/RecResource/Activities.tsx
+++ frontend/src/components/RecResource/Activities.tsx
@@ -21,68 +21,62 @@ import horsebackRidingIcon from '@/images/activities/horseback-riding.svg';
 import '@/styles/components/Activities.scss';
 
 interface ActivityProps {
-  activities: { recreation_activity_code: string }[];
+  activities: {
+    description: string;
+    recreation_activity_code: string;
+  }[];
 }
 
 interface ActivityMap {
-  [key: string]: {
-    icon: string;
-    description: string;
-  };
+  [key: string]: string;
 }
 
 const activityMap: ActivityMap = {
-  '01': { icon: fishingIcon, description: 'Angling' },
-  '02': { icon: boatLaunchIcon, description: 'Boating' },
-  '03': { icon: canoeingIcon, description: 'Canoeing' },
-  '04': { icon: kayakingIcon, description: 'Kayaking' },
-  '05': { icon: scubaDivingIcon, description: 'Scuba Diving or Skin Diving' },
-  '06': { icon: waterskiingIcon, description: 'Waterskiing' },
-  '07': { icon: swimmingIcon, description: 'Swimming & Bathing' },
-  // '08': { icon: beachActivitiesIcon, description: 'Beach Activities' },
-  '09': { icon: picnicAreaIcon, description: 'Picnicking' },
-  '10': { icon: huntingIcon, description: 'Hunting' },
-  '11': { icon: cavingIcon, description: 'Caving' },
-  '12': { icon: hikingIcon, description: 'Hiking' },
-  // '13': { icon: 'mountaineering', description: 'Mountaineering' },
-  // '14': { icon: 'nature-study', description: 'Nature Study' }, -- is this correct?
-  // '15': { icon: 'orienteering', description: 'Orienteering' },
-  '16': { icon: viewpointIcon, description: 'Viewing' },
-  '17': { icon: wildlifeViewingIcon, description: 'Wildlife Viewing' },
-  // '18': { icon: 'gathering-and-collecting', description: 'Gathering and Collecting' },
-  '19': { icon: horsebackRidingIcon, description: 'Horseback Riding' },
-  // '20': { icon: 'atv', description: 'Trailbike Riding (Motorized)' }, -- is this correct?
-  // '21': { icon: '4-wheel-driving', description: '4-Wheel Driving' },
-  '22': { icon: snowmobilingIcon, description: 'Snowmobiling' },
-  // '23': { icon: 'snowshoeing', description: 'Snowshoeing' },
-  // '24': { icon: 'skiing', description: 'Skiing' },
-  // '25': { icon: 'icefishing', description: 'Icefishing' },
-  // '26': { icon: 'other', description: 'Other' },
-  '27': { icon: mountainBikingIcon, description: 'Mountain Biking' },
-  '28': { icon: climbingIcon, description: 'Climbing' },
-  '29': { icon: sailingIcon, description: 'Sailing' },
-  // '30': { icon: 'wind-surfing', description: 'Wind Surfing' },
-  // '31': { icon: 'rafting', description: 'Rafting' },
-  '32': { icon: campingIcon, description: 'Camping' }, // is this correct?
-  // '33': { icon: 'ski-touring', description: 'Ski Touring' },
+  '01': fishingIcon,
+  '02': boatLaunchIcon,
+  '03': canoeingIcon,
+  '04': kayakingIcon,
+  '05': scubaDivingIcon,
+  '06': waterskiingIcon,
+  '07': swimmingIcon,
+  // '08': beachActivitiesIcon,
+  '09': picnicAreaIcon,
+  '10': huntingIcon,
+  '11': cavingIcon,
+  '12': hikingIcon,
+  // '13': mountaineeringIcon,
+  // '14': natureStudyIcon,
+  // '15': orienteeringIcon,
+  '16': viewpointIcon,
+  '17': wildlifeViewingIcon,
+  // '18': gatheringAndCollectingIcon,
+  '19': horsebackRidingIcon,
+  // '20': atvIcon,
+  // '21': fourWheelDrivingIcon,
+  '22': snowmobilingIcon,
+  // '23': snowshoeingIcon,
+  // '24': skiingIcon,
+  // '25': icefishingIcon,
+  // '26': otherIcon,
+  '27': mountainBikingIcon,
+  '28': climbingIcon,
+  '29': sailingIcon,
+  //'30': campingIcon,
+  // '31': raftingIcon,
+  '32': campingIcon,
+  // '33': skiTouringIcon,
 };
 
 const Activities = ({ activities }: ActivityProps) => {
   return (
     <ul className="activities-list">
-      {activities.map((activityCode, index: number) => {
-        const { recreation_activity_code } = activityCode;
-        const activity = activityMap?.[recreation_activity_code];
-        if (!activity) return null;
+      {activities.map((activity) => {
+        const { description, recreation_activity_code } = activity;
+        const activityIcon = activityMap?.[recreation_activity_code];
+        if (!activityIcon) return null;
         return (
-          <li key={index}>
-            <img
-              key={index}
-              alt={activity.description}
-              src={activity.icon}
-              height={32}
-              width={32}
-            />
+          <li key={description} className="activity-icon">
+            <img alt={description} src={activityIcon} height={32} width={32} />
           </li>
         );
       })}
