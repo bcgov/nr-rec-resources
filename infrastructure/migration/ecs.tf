e453cae08d23210004315897005c0dfa5a096da0
marcellmueller <marcel@button.is>
2025-01-24 16:25:42 -0800

    chore: move flyway ecs task to reusable module

--- infrastructure/migration/ecs.tf
+++ infrastructure/migration/ecs.tf
@@ -36,121 +36,52 @@ resource "terraform_data" "trigger_deployment" {
   input = "${timestamp()}"
 }
 
-resource "aws_ecs_task_definition" "flyway_task" {
-  family                   = "${var.app_name}-flyway-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = data.aws_iam_role.app_container_role.arn
-  container_definitions = jsonencode([
-    {
-      name      = "${var.app_name}-flyway"
-      image     = "${var.flyway_image}"
-      essential   = true
-      environment = [
-        {
-          name = "APP_ENV"
-          value = local.rds_app_env
-        },
-        {
-          name  = "FLYWAY_URL"
-          value = "jdbc:postgresql://${data.aws_rds_cluster.rds_cluster.endpoint}/${var.db_name}"
-        },
-        {
-          name  = "FLYWAY_USER"
-          value = local.db_master_creds.username
-        },
-        {
-          name  = "FLYWAY_PASSWORD"
-          value = local.db_master_creds.password
-        },
-        {
-          name  = "FLYWAY_DEFAULT_SCHEMA"
-          value = "${var.db_schema}"
-        },
-        {
-          name  = "FLYWAY_CONNECT_RETRIES"
-          value = "2"
-        },
-        {
-          name  = "FLYWAY_GROUP"
-          value = "true"
-        },
-        {
-          # Enable Flyway clean
-          name = "FLYWAY_CLEAN_DISABLED"
-          value = "false"
-        }
-      ]
+module "flyway_task" {
+  source = "../modules/ecs-flyway-task"
 
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-create-group  = "true"
-          awslogs-group         = "/ecs/${var.app_name}/flyway"
-          awslogs-region        = var.aws_region
-          awslogs-stream-prefix = "ecs"
-        }
+  app_name           = var.app_name
+  flyway_image       = var.flyway_image
+  cpu                = "512"
+  memory             = "1024"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = data.aws_iam_role.app_container_role.arn
+  environment = [
+      {
+        name = "APP_ENV"
+        value = local.rds_app_env
+      },
+      {
+        name  = "FLYWAY_URL"
+        value = "jdbc:postgresql://${data.aws_rds_cluster.rds_cluster.endpoint}/${var.db_name}"
+      },
+      {
+        name  = "FLYWAY_USER"
+        value = local.db_master_creds.username
+      },
+      {
+        name  = "FLYWAY_PASSWORD"
+        value = local.db_master_creds.password
+      },
+      {
+        name  = "FLYWAY_DEFAULT_SCHEMA"
+        value = "${var.db_schema}"
+      },
+      {
+        name  = "FLYWAY_CONNECT_RETRIES"
+        value = "2"
+      },
+      {
+        name  = "FLYWAY_GROUP"
+        value = "true"
+      },
+      {
+        # Enable Flyway clean
+        name = "FLYWAY_CLEAN_DISABLED"
+        value = "false"
       }
-      mountPoints = []
-      volumesFrom = []
-
-    }
-  ])
-  lifecycle {
-    replace_triggered_by = [terraform_data.trigger_deployment]
-  }
-  provisioner "local-exec" {
-  command = <<-EOF
-    task_arn=$(aws ecs run-task \
-      --task-definition ${var.app_name}-flyway-task \
-      --cluster ${data.aws_ecs_cluster.ecs_cluster.id} \
-      --count 1 \
-      --network-configuration awsvpcConfiguration={securityGroups=[${data.aws_security_group.app.id}],subnets=${data.aws_subnets.app.ids[0]},assignPublicIp=DISABLED} \
-      --query 'tasks[0].taskArn' \
-      --output text)
-
-    echo "Flyway task started with ARN: $task_arn at $(date)."
-
-    echo "Waiting for Flyway task to complete..."
-    aws ecs wait tasks-stopped --cluster ${data.aws_ecs_cluster.ecs_cluster.id} --tasks $task_arn
-
-    echo "Flyway task completed at $(date)."
-
-    task_status=$(aws ecs describe-tasks --cluster ${data.aws_ecs_cluster.ecs_cluster.id} --tasks $task_arn --query 'tasks[0].lastStatus' --output text)
-    echo "Flyway task status: $task_status at $(date)."
-    log_stream_name=$(aws logs describe-log-streams \
-      --log-group-name "/ecs/${var.app_name}/flyway" \
-      --order-by "LastEventTime" \
-      --descending \
-      --limit 1 \
-      --query 'logStreams[0].logStreamName' \
-      --output text)
-
-    echo "Fetching logs from log stream: $log_stream_name"
-
-    aws logs get-log-events \
-      --log-group-name "/ecs/${var.app_name}/flyway" \
-      --log-stream-name $log_stream_name \
-      --limit 1000 \
-      --no-cli-pager
-
-    # Fetch the task's exit code
-    task_exit_code=$(aws ecs describe-tasks \
-      --cluster ${data.aws_ecs_cluster.ecs_cluster.id} \
-      --tasks $task_arn \
-      --query 'tasks[0].containers[0].exitCode' \
-      --output text)
-
-    if [ "$task_exit_code" != "0" ]; then
-      echo "Flyway task failed with exit code: $task_exit_code"
-
-      exit 1
-    fi
-
-    echo "Flyway task succeeded with exit code: $task_exit_code."
-  EOF
-}
+  ]
+  aws_region      = var.aws_region
+  cluster_id      = data.aws_ecs_cluster.ecs_cluster.id
+  security_group  = data.aws_security_group.app.id
+  subnet          = data.aws_subnets.app.ids[0]
 }
