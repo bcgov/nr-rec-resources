809561f09799656c28281951e8a227c7a0b0e36e
marcellmueller <marcel@button.is>
2025-01-28 12:54:36 -0800

    test: update frontend tests due to database id changes

--- frontend/src/components/rec-resource/Status.test.tsx
+++ frontend/src/components/rec-resource/Status.test.tsx
@@ -3,21 +3,21 @@ import Status from '@/components/rec-resource/Status';
 
 describe('the Status component', () => {
   it('renders the blue status icon', async () => {
-    render(<Status description="Open" statusCode="01" />);
+    render(<Status description="Open" statusCode={1} />);
     const statusElement = screen.getByAltText(/Site Open status icon/i);
 
     expect(statusElement).toBeInTheDocument();
   });
 
   it('renders the red status icon', async () => {
-    render(<Status description="Closed" statusCode="02" />);
+    render(<Status description="Closed" statusCode={2} />);
     const statusElement = screen.getByAltText(/Site Closed status icon/i);
 
     expect(statusElement).toBeInTheDocument();
   });
 
   it('does not render an icon for an unknown status code', async () => {
-    render(<Status description="Unknown" statusCode="03" />);
+    render(<Status description="Unknown" statusCode={3} />);
     const statusElement = screen.queryByAltText(/Site Unknown status icon/i);
 
     expect(statusElement).not.toBeInTheDocument();
