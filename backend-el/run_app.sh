18e31b0facb8894298e2f345948a5914c3faa243
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-01-28 14:25:49 -0800

    chore: try running without init, init will be deleted later

--- backend-el/run_app.sh
+++ backend-el/run_app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-keytool -list -v -keystore /app/cert/jssecacerts -alias "${DB_HOST}" -storepass "${CERT_SECRET}"
+cert_folder="/opt"
+openssl s_client -connect "${DB_HOST}:${DB_PORT}" -showcerts </dev/null | openssl x509 -outform pem >"$cert_folder/${DB_HOST}.pem" || exit 1
+openssl x509 -outform der -in "$cert_folder/${DB_HOST}.pem" -out "$cert_folder/${DB_HOST}.der" || exit 1
+keytool -import -alias "orakey-${DB_HOST}-1" -keystore "${JAVA_HOME}"/lib/security/cacerts -storepass changeit -file "$cert_folder/${DB_HOST}.der" -noprompt || exit 1
 echo "JDBC URL is ${JDBC_URL}"
 echo "Starting Java APP"
 java -Djavax.net.debug=ssl:handshake -Duser.name=REC-ORACLE-EL -Xms512m -Xmx512m -jar app.jar
