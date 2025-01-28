80173be322077109f49450e961a6e39fa6e9f44d
marcellmueller <marcel@button.is>
2025-01-28 08:26:11 -0800

    refactor: convert string codes to integers and update fixtures

--- migrations/fta/sql/V1.0.2__fta_to_rst.sql
+++ migrations/fta/sql/V1.0.2__fta_to_rst.sql
@@ -19,18 +19,19 @@ on
     rp.forest_file_id = rc.forest_file_id
 on conflict do nothing;
 
-insert into rst.recreation_activity_code (recreation_activity_code, description)
-select
-    rac.recreation_activity_code,
-    rac.description as description
-from
-    fta.recreation_activity_code rac;
+-- insert into rst.recreation_activity_code (recreation_activity_code, description)
+-- select
+--     rac.recreation_activity_code,
+--     rac.description as description
+-- from
+--     fta.recreation_activity_code rac;
 
 
 insert into rst.recreation_activity (rec_resource_id, recreation_activity_code)
 select
     ra.forest_file_id as rec_resource_id,
-    ra.recreation_activity_code
+    -- Convert strings codes ie '01', '02' to integers
+    cast(ra.recreation_activity_code as int) as recreation_activity_code
 from
     fta.recreation_activity ra;
 
@@ -38,8 +39,8 @@ insert into rst.recreation_status (rec_resource_id, status_code, comment)
 select
     forest_file_id,
     case
-        when closure_ind = 'Y' then '02' -- Closed
-        when closure_ind = 'N' then '01' -- Open
+        when closure_ind = 'Y' then 2 -- Closed
+        when closure_ind = 'N' then 1 -- Open
     end as recreation_status_code,
     project_comment as description
 from
