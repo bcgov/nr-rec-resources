b332bf5b0e285a186f960e0f28348dd572b37337
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-01-28 12:05:40 -0800

    chore: support openshift deployment, dev and prod, configure aws secret as separate, aws secret sync will be added later

--- backend-el/src/main/java/ca/bc/gov/nrs/environment/fta/el/FtaRstExporterApplication.java
+++ backend-el/src/main/java/ca/bc/gov/nrs/environment/fta/el/FtaRstExporterApplication.java
@@ -1,20 +1,17 @@
 package ca.bc.gov.nrs.environment.fta.el;
 
-import ca.bc.gov.nrs.environment.fta.el.services.ApplicationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
+
+import ca.bc.gov.nrs.environment.fta.el.services.ApplicationService;
 
 @SpringBootApplication
 @EnableRetry
 public class FtaRstExporterApplication implements CommandLineRunner {
 
   private final ApplicationService applicationService;
-  private static final Logger logger = LoggerFactory
-    .getLogger(FtaRstExporterApplication.class);
 
   public FtaRstExporterApplication(ApplicationService applicationService) {
     this.applicationService = applicationService;
