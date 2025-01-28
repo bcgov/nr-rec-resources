66eb2d20be274930ce6499b41f34c03a8f4ea209
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-01-28 15:51:16 -0800

    chore: remove init container and related charts, add certs to the main container, it runs as a cronjob and not long running process

--- backend-el/run_app.sh
+++ backend-el/run_app.sh
@@ -5,4 +5,10 @@ openssl x509 -outform der -in "$cert_folder/${DB_HOST}.pem" -out "$cert_folder/$
 keytool -import -alias "orakey-${DB_HOST}-1" -keystore "${JAVA_HOME}"/lib/security/cacerts -storepass changeit -file "$cert_folder/${DB_HOST}.der" -noprompt || exit 1
 echo "JDBC URL is ${JDBC_URL}"
 echo "Starting Java APP"
-java -Djavax.net.debug=ssl:handshake -Duser.name=REC-ORACLE-EL -Xms512m -Xmx512m -jar app.jar
+# if debug ssl handshake is true add -Djavax.net.debug=ssl:handshake else not needed.
+if [ -n "${SSL_DEBUG}" ]; then
+  java -Djavax.net.debug=ssl:handshake -Duser.name=REC-ORACLE-EL -Xms512m -Xmx512m -jar app.jar
+else
+  java -Duser.name=REC-ORACLE-EL -Xms512m -Xmx512m -jar app.jar
+fi
+``
