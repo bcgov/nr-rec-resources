fc40df546178e05bee62c459d25fa7effba3f29b
marcellmueller <marcel@button.is>
2025-01-30 15:59:36 -0800

    chore: fix recreation resource filter dto

--- frontend/src/components/search/filters/FilterGroup.tsx
+++ frontend/src/components/search/filters/FilterGroup.tsx
@@ -8,20 +8,20 @@ import { Filter } from '@/components/search/types';
 import '@/components/search/filters/Filters.scss';
 
 interface FilterGroupProps {
-  title?: string;
-  filters: Filter[];
+  label?: string;
+  options: Filter[];
   param: string;
   showMoreBtn?: boolean;
 }
 
 const FilterGroup = ({
-  filters,
+  options,
   param,
-  title,
+  label,
   showMoreBtn = true,
 }: FilterGroupProps) => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [showAllFilters, setShowAllFilters] = useState(false);
+  const [showAllOptions, setShowAllFilters] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { id, checked } = event.target;
@@ -44,17 +44,17 @@ const FilterGroup = ({
     }
   };
 
-  const filtersCount = filters?.length + 1;
-  const isShowAllFilters = filtersCount > 5 && !showAllFilters && showMoreBtn;
-  const filterList = isShowAllFilters ? filters.slice(0, 5) : filters;
+  const filtersCount = options?.length + 1;
+  const isShowAllFilters = filtersCount > 5 && !showAllOptions && showMoreBtn;
+  const optionList = isShowAllFilters ? options.slice(0, 5) : options;
   const filterParamsArray = searchParams.get(param)?.split('_');
 
   return (
     <div className="filter-group-container">
-      <span className="filter-group-title">{title}</span>
+      <span className="filter-group-title">{label}</span>
       <Form.Group className="filter-options-container">
-        {filterList?.map((filter) => {
-          const { count, description, id } = filter;
+        {optionList?.map((option) => {
+          const { count, description, id } = option;
           const isDefaultChecked = filterParamsArray?.includes(String(id));
           const isDisabled = count === 0;
 
@@ -75,16 +75,16 @@ const FilterGroup = ({
         <button
           className="show-all-link"
           onClick={() => {
-            setShowAllFilters(!showAllFilters);
+            setShowAllFilters(!showAllOptions);
           }}
           onKeyDown={(e) => {
             if (e.key === 'Enter' || e.key === ' ') {
               e.preventDefault();
-              setShowAllFilters(!showAllFilters);
+              setShowAllFilters(!showAllOptions);
             }
           }}
         >
-          {showAllFilters ? (
+          {showAllOptions ? (
             <>
               Show less
               <FontAwesomeIcon icon={faChevronUp} />
