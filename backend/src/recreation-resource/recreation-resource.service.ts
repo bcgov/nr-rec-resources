025e104a7400b2f60cf7979b65d7f8bf7c3de19b
marcellmueller <marcel@button.is>
2025-02-03 09:39:05 -0800

    test: update recreation-resource api service tests for filtering changes

--- backend/src/recreation-resource/recreation-resource.service.ts
+++ backend/src/recreation-resource/recreation-resource.service.ts
@@ -95,13 +95,12 @@ export class RecreationResourceService {
       },
       where: {
         rec_resource_id: {
-          in: totalRecordIds.map((record) => record.rec_resource_id),
+          in: totalRecordIds?.map((record) => record.rec_resource_id),
         },
       },
     });
-
     // Merge and include missing entries with a count of 0
-    const activityCount = allActivityCodes.map(
+    const activityCount = allActivityCodes?.map(
       (activity: ActivityGetPayload) => {
         const matchedGroup = groupActivities.find(
           (group) =>
@@ -196,7 +195,7 @@ export class RecreationResourceService {
       data: this.formatResults(recreationResources),
       page,
       limit,
-      total: totalRecordIds.length,
+      total: totalRecordIds?.length,
       filters: [
         {
           type: "multi-select",
