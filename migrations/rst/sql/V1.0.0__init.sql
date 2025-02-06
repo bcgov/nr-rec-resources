0b8a2233ecef8bd1bd7fc6899969185df27eb16c
marcellmueller <marcel@button.is>
2025-02-06 11:18:22 -0800

    chore: fix rebase migration error

--- migrations/rst/sql/V1.0.0__init.sql
+++ migrations/rst/sql/V1.0.0__init.sql
@@ -20,26 +20,3 @@ comment on column rst.recreation_resource.name is 'Name of the Recreation Projec
 comment on column rst.recreation_resource.site_location is 'A text description generally describing the closest community or, for more isolated sites and trails, it could be a geographic feature to a recreation site or trail. e.g. VERNON, KELOWNA, PRINCE GEORGE.';
 
 comment on column rst.recreation_resource.rec_resource_type is 'Code representing a specific feature associated with the recreation resource.';
-
-create table rst.recreation_activity_code (
-    recreation_activity_code varchar(3) primary key,
-    description varchar(120)
-);
-
-comment on table rst.recreation_activity_code is 'Activity code types for recreation projects.';
-
-comment on column rst.recreation_activity_code.recreation_activity_code is 'Code describing the Recreation Activity.';
-
-comment on column rst.recreation_activity_code.description is 'Description of the code value.';
-
-create table if not exists rst.recreation_activity (
-    id serial primary key, -- This is a surrogate key to make Prisma happy
-    rec_resource_id varchar(200) not null references rst.recreation_resource (rec_resource_id),
-    recreation_activity_code varchar(3) not null references rst.recreation_activity_code (recreation_activity_code)
-);
-
-comment on table rst.recreation_activity is 'The types of available activities for a given project.';
-
-comment on column rst.recreation_activity.rec_resource_id is 'File identification assigned to Provincial Forest Use files. Assigned file number. Usually the Licence, Tenure or Private Mark number.';
-
-comment on column rst.recreation_activity.recreation_activity_code is 'Code describing the Recreation Activity.';
