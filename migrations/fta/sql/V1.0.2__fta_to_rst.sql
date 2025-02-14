6b2ad154f413ebcc32e99d15eee373cf2d7083aa
marcellmueller <marcel@button.is>
2025-02-14 14:37:58 -0800

    chore: test deployment

--- migrations/fta/sql/V1.0.2__fta_to_rst.sql
+++ migrations/fta/sql/V1.0.2__fta_to_rst.sql
@@ -13,10 +13,11 @@ insert into
         description,
         closest_community,
         display_on_public_site,
-        rec_resource_type
+        rec_resource_type,
+        district_code
     )
 select
-    rp.forest_file_id,
+    rp.forest_file_id as rec_resource_id,
     rp.project_name as name,
     case
         when rc.rec_comment_type_code = 'DESC' then rc.project_comment
@@ -24,36 +25,26 @@ select
     end as description,
     rp.site_location as closest_community,
     case
-        when rp.recreation_view_ind = 'Y' then TRUE
-        else FALSE
+        when rp.recreation_view_ind = 'Y' then true
+        else false
     end as display_on_public_site,
-    rmf.recreation_map_feature_code
+    rmf.recreation_map_feature_code as rec_resource_type,
+    coalesce(c.campsite_count, 0) as campsite_count,
+    xref.recreation_district_code as district_code
 from
     fta.recreation_project rp
     left join fta.recreation_comment rc on rp.forest_file_id = rc.forest_file_id
     left join fta.recreation_map_feature rmf on rp.forest_file_id = rmf.forest_file_id
-on conflict do nothing;
-
-
-insert into
-    rst.recreation_campsite (
-        rec_resource_id,
-        campsite_count
-    )
-select
-    rp.forest_file_id,
-    coalesce(c.campsite_count, 0) as campsite_count
-from
-    fta.recreation_project rp
     left join (
         select
             forest_file_id,
-            COUNT(*) as campsite_count
-        from fta.recreation_defined_campsite
-        group by forest_file_id
+            count(*) as campsite_count
+        from
+            fta.recreation_defined_campsite
+        group by
+            forest_file_id
     ) c on rp.forest_file_id = c.forest_file_id
-on conflict do nothing;
-
+    left join fta.recreation_district_xref xref on rp.forest_file_id = xref.forest_file_id on conflict (rec_resource_id) do nothing;
 
 insert into
     rst.recreation_activity (rec_resource_id, recreation_activity_code)
@@ -78,21 +69,22 @@ from
 where
     rec_comment_type_code = 'CLOS';
 
-insert into rst.recreation_fee (
-    rec_resource_id,
-    fee_amount,
-    fee_start_date,
-    fee_end_date,
-    monday_ind,
-    tuesday_ind,
-    wednesday_ind,
-    thursday_ind,
-    friday_ind,
-    saturday_ind,
-    sunday_ind,
-    recreation_fee_code,
-    fee_description
-)
+insert into
+    rst.recreation_fee (
+        rec_resource_id,
+        fee_amount,
+        fee_start_date,
+        fee_end_date,
+        monday_ind,
+        tuesday_ind,
+        wednesday_ind,
+        thursday_ind,
+        friday_ind,
+        saturday_ind,
+        sunday_ind,
+        recreation_fee_code,
+        fee_description
+    )
 select
     rf.forest_file_id as rec_resource_id,
     rf.fee_amount,
@@ -107,7 +99,6 @@ select
     rf.sunday_ind,
     rf.recreation_fee_code,
     rfc.description as fee_description
-from fta.recreation_fee rf
-left join fta.recreation_fee_code rfc
-    on rf.recreation_fee_code = rfc.recreation_fee_code
-on conflict do nothing;
+from
+    fta.recreation_fee rf
+    left join fta.recreation_fee_code rfc on rf.recreation_fee_code = rfc.recreation_fee_code on conflict do nothing;
