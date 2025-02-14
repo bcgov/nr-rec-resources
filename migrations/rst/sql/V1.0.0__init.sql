0ffa7cba6d998c6504ead2276782fa41719e29f7
marcellmueller <marcel@button.is>
2025-02-13 16:27:08 -0800

    chore: add recreation_district_code

--- migrations/rst/sql/V1.0.0__init.sql
+++ migrations/rst/sql/V1.0.0__init.sql
@@ -7,6 +7,11 @@ create table if not exists rst.recreation_resource_type_code (
     description varchar(200)
 );
 
+create table if not exists rst.recreation_district_code (
+    district_code varchar(4) primary key,
+    description varchar(120) not null
+);
+
 create table if not exists rst.recreation_resource (
     rec_resource_id varchar(200) not null primary key,
     name varchar(200),
@@ -14,7 +19,8 @@ create table if not exists rst.recreation_resource (
     closest_community varchar(200),
     display_on_public_site boolean default false,
     campsite_count integer default 0,
-    rec_resource_type varchar(10) not null references rst.recreation_resource_type_code (rec_resource_type_code)
+    rec_resource_type varchar(10) not null references rst.recreation_resource_type_code (rec_resource_type_code),
+    district_code varchar(4) references rst.recreation_district_code (district_code)
 );
 
 comment on table rst.recreation_resource is 'Resource information relating to a recreational file. A recreation file can have only one resource. A recreation resource must be of type Site, Reserve, Trail, or Interpretive Forest.';
@@ -28,3 +34,5 @@ comment on column rst.recreation_resource.closest_community is 'A text descripti
 comment on column rst.recreation_resource.rec_resource_type is 'Code representing a specific feature associated with the recreation resource.';
 
 comment on column rst.recreation_resource.campsite_count is 'Number of campsites available in the recreation site or trail.';
+
+comment on column rst.recreation_resource.district_code is 'A code indicating a recreation district boundary. EG Cascades, Chilliwack, Discovery Coast. Note: There are 17 Recreation District Boundaries, most of which have different geographical boundaries than Forest District Boundaries. Because of this, Rec District Boundary Codes are different from Forest District Boundary Codes.';
