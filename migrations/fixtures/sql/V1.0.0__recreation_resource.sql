1e80aee0964e95c81ea3bd8510c9817ca8b81b4f
marcellmueller <marcel@button.is>
2025-02-18 10:25:54 -0800

    refactor: recreation_resource_type is one to many table

--- migrations/fixtures/sql/V1.0.0__recreation_resource.sql
+++ migrations/fixtures/sql/V1.0.0__recreation_resource.sql
@@ -13,7 +13,6 @@ insert into
         description,
         closest_community,
         display_on_public_site,
-        rec_resource_type,
         campsite_count,
         district_code
     )
@@ -24,7 +23,6 @@ values
         'The Zero K Tulameen Snowmobile Parking Lot is an area managed for snowmobilers who use the Mt. Henning and 10 K Area Snowmobile Trails in the vicinity. The parking lot and the trail network it supports is managed under a partnership agreement with the Coquihalla Summit Snowmobile Club during the snowmobile season and has an informative kiosk, outhouse and fee collection hut complete with a heated change room. Fees are applicable for the use of the snowmobile trails which are frequently groomed.',
         'MERRITT',
         true,
-        'RTR',
         10,
         'RDKA'
     ),
@@ -34,7 +32,6 @@ values
         'Site not currently being managed. Pending disestablishment (Jan 2009) - Sept 2021 Update: Site should have updated inspection. Site was never disestablished and is well used by the public.',
         'PRINCE GEORGE',
         true,
-        'RTR',
         12,
         'RDPG'
     ),
@@ -44,7 +41,6 @@ values
         'This snowmobile emergency cabin is located north of Coquihalla Mountain and is accessed via the Mt. Henning and 10 k Area Snowmobile Trails. The cabin is maintained under management agreement with the Coquihalla Summit Snowmobile Club and is intended for emergency use only. Please respect this cabin by keeping it clean and vandalism free. An avalanche terrain map and trail map is located within the cabin to advise snowmobilers of the types of avalanche terrain encountered in the area and the avalanche danger rating system.',
         'MERRITT',
         true,
-        'RTR',
         90,
         'RDKA'
     ),
@@ -54,7 +50,6 @@ values
         'A 1 hectare gravel parking lot located at 10.2 km of the Tulameen Forest Service Road. Managed in partnership with the Coquihalla Summit Snowmobile Club this parking lot can be used by sledders who generally access the Coquihalla Mountain area near where the 10 k Cabin is located. The parking lot may be ploughed some winter seasons while other seasons it will not be ploughed depending upon whether the industrial road is being used and machinery can then access the lot for ploughing. Best to check the club website for more details at Coqsnow.com',
         'MERRITT',
         true,
-        'RTR',
         1,
         'RDKA'
     ),
@@ -64,7 +59,6 @@ values
         'Come and hike the original route used by Goldrush miners as they travelled overland from the Coast into the Goldfields of the Barkerville area. The trailhead is in Barkerville and travels up and over Yanks Peak (French Snowshoe Mountain) and down to Weaver Creek where it joins the current road system. There are numerous historic sites along the trail as well as several rustic campsites. Visit www.barkerville.bc.ca for more details. Non Motorized Use Only',
         'WELLS',
         true,
-        'RTR',
         8,
         'RDVA'
     ),
@@ -74,7 +68,6 @@ values
         '18 mile camping area sits at an elevation +/- 587 meters on Revelstoke Lake with changing shore lines, Revelstoke Lake is a lake created by hydro electric power generation.',
         'REVELSTOKE',
         true,
-        'RTR',
         12,
         'RDRM'
     ),
@@ -84,7 +77,6 @@ values
         'An area set aside for any future snowmobile parking lot or shelter location which is currently managed under a snowmobile trail maintenance agreement with the Merritt Snowmobile Club. Currently has only an outhouse on location.',
         'MERRITT',
         true,
-        'RTR',
         14,
         'RDKA'
     ),
@@ -94,7 +86,6 @@ values
         'The West Kootenay Snow Goers manage about 30 Km of groomed trail in the area with 2 day use warming huts. The groomed trails are easy riding and the whole area is generally for beginner to moderate sledding skills with some areas of advanced terrain around the Mount Shields area.',
         'CASTLEGAR',
         true,
-        'RTR',
         19,
         'RDKB'
     ),
@@ -104,7 +95,6 @@ values
         'Forested Mtn Bike',
         'WHISTLER',
         true,
-        'RTR',
         4,
         'RDSQ'
     ),
@@ -114,7 +104,6 @@ values
         'A small scenic site at the confluence of Quinn Creek and the Bull River.',
         'FERNIE',
         true,
-        'RR',
         9,
         'RDKB'
     ),
@@ -124,7 +113,6 @@ values
         'The trail starts at 70 Mile House BC and connects to a vast network of snowmobile and ATV trails around south Green Lake. From 70 Mile House, it is also possible to connect to the Gold Rush Recreation Trail, allowing users to travel as far north as Horsefly, and as far south as Clinton.',
         '70 MILE HOUSE',
         true,
-        'RR',
         9,
         'RDMH'
     ),
@@ -134,7 +122,6 @@ values
         'This is a small site, semi-forested with mature Larch and Lodgepole Pine trees, with a central grassy, open area. Most of the sites are well-shaded, as the recreation site is nestled in a valley bottom. The site is located adjacent to Seven Mile Lake, a small shallow, weedy lake which is the headwaters of Caven Creek. The lake has a steep shoreline and is treed right to the water on the south shore, but a rough trail along the north side of the lake provides access to the deeper portion of the lake which is suitable for fishing. The lake is stocked with Cutthroat trout every other year. The site is located right on the main FSR but traffic is not excessive at this point.',
         'CRANBROOK',
         true,
-        'RR',
         8,
         'RDKB'
     ),
@@ -144,7 +131,6 @@ values
         'These are historic cabin/shelters sites, and may not be an actual functioning shelter.',
         'Bob Quinn Lake',
         true,
-        'RTR',
         66,
         'RDSI'
     ),
@@ -154,7 +140,6 @@ values
         'The 99 Mile Cross Country Ski Trails are groomed and maintained by the 100 Mile Nordic Ski Society. Please visit their website at 100 Mile Nordics for more information regarding seasons passes, day passes, lodge operating hours, and ski conditions. The trail network boasts 45 km of groomed ski trails, 5 km of lit trails for night skiing and 6.5 km of snowshoe trails.',
         '100 MILE HOUSE',
         true,
-        'RTR',
         45,
         'RDOS'
     ),
@@ -164,7 +149,6 @@ values
         'These trails wind their way through stands of Douglas-fir and lodgepole pine above the community of 100 Mile House. The 99 Mile Hill is home to several trail networks for a variety of recreation activities, including cross-country skiing, snowmobiling, and horseback riding. The area also provides opportunities for the public to tour through a demonstration forest with interpretative signs.',
         '100 MILE HOUSE',
         true,
-        'RTR',
         1,
         'RDOS'
     ),
@@ -174,7 +158,6 @@ values
         'off-road motorcycle trails (singletrack)',
         '100 MILE HOUSE',
         true,
-        'RTR',
         0,
         'RDOS'
     ),
@@ -184,7 +167,6 @@ values
         'This is a parking area for those enjoying the recreation trails.',
         '100 Mile House',
         true,
-        'RTR',
         8,
         'RDOS'
     ),
@@ -194,7 +176,6 @@ values
         'These Trails are a short Drive outside of 100 Mile house',
         '100 Mile House',
         true,
-        'RTR',
         9,
         'RDOS'
     ),
@@ -204,7 +185,6 @@ values
         'These snowshoe trails are near the 99 Mile Cross Country Ski Trails network. There are 2 snowshoe trail loops available: the Bear Paw trail and the Beanstalk Cabin trail. The Bear Paw trail is the shorter and easier loop, while the Beanstalk Cabin trail is the longer and more challenging trail. Please do not walk or snowshoe along the set cross-country ski trails in the area.',
         '100 MILE HOUSE',
         true,
-        'RTR',
         23,
         'RDOS'
     ),
@@ -214,7 +194,6 @@ values
         'Parking area',
         '100 Mile House',
         true,
-        'RTR',
         12,
         'RDOS'
     ),
@@ -224,7 +203,6 @@ values
         'Looking for sun on the beach? Abbott Creek is the spot. A mid-sized rec site along the North Shore of Quesnel Lake with the majority of the sites set back in the shade of the Cottonwood trees along the large gravel beach. Boat launch is across the beach, 4x4 may be required. This also makes an excellent spot to start a boat trip on Quesnel Lake. Please park in the upper parking area and not a campsite if you intend to be away from the site for an extended period.',
         'LIKELY',
         true,
-        'RTR',
         13,
         'RDPG'
     ),
@@ -234,7 +212,6 @@ values
         'This small 3 vehicle somewhat remote recreation site is located in a semi open area on the north east side of Abbott Lake and is popular for fishing and boating. The camping spots are suitable for small truck campers and tenters and not for anything towed due to the rough access.',
         'MERRITT',
         true,
-        'RTR',
         12,
         'RDKA'
     ),
@@ -244,7 +221,6 @@ values
         'This is a 400m access trail to the south shore of Abbott Lake. There are no facilities other than a parking area off of the Horsefly Road. There is no boat launching access via this trail.',
         'HORSEFLY',
         true,
-        'RTR',
         12,
         'RDPG'
     ),
@@ -254,7 +230,6 @@ values
         'Aberdeen Columns is a climbing area in central BC. The Basalt columns are an ancient artifact of volcanic activity long ago over the Aberdeen plateau. Rock climbing is a dangerous activity carrying a significant risk to your personal safety and should be undertaken with a full understanding of all inherent risks.',
         'LAVINGTON',
         true,
-        'RTR',
         12,
         'RDKA'
     ),
@@ -264,7 +239,6 @@ values
         'Aberdeen columns are basalt rock formations. The Okanagan valley formed 60 million years ago as a result from a rift. 40 million years ago the valley was a wash with gushing volcanoes. Some of which are Knox Mountain, Dilworth and Mount Boucherie. The valley didn''t exist when dinosaurs roamed the earth. The result today is a popular climbing area.',
         'LAVINGTON',
         true,
-        'RTR',
         9,
         'RDKA'
     ),
@@ -274,7 +248,6 @@ values
         'This semi-open site on a medium sized fishing lake is subject to significant water level fluctuations. The access is very rough for 2 km before the site.',
         'LAVINGTON',
         true,
-        'RTR',
         90,
         'RDKA'
     ),
@@ -284,7 +257,6 @@ values
         null,
         'LAVINGTON',
         true,
-        'RTR',
         12,
         'RDKA'
     ),
@@ -294,7 +266,6 @@ values
         'Snowshoe and hiking trails, XC trails short drive from Lavington BC',
         'LUMBY',
         true,
-        'SIT',
         45,
         'RDCC'
     ),
@@ -304,7 +275,6 @@ values
         'Forested Trail',
         'SILVERTON',
         true,
-        'SIT',
         6,
         'RDCC'
     ),
@@ -314,7 +284,6 @@ values
         'A small day-use site with 3 tables and 1 outhouse. Located on the north shore of Tchesinkut Lake.',
         'BURNS LAKE',
         true,
-        'SIT',
         12,
         'RDCC'
     ),
@@ -324,7 +293,6 @@ values
         'This trail provides access to a small secluded lake with excellent fishing. Agnes Lake is situated in a lodgepole pine & spruce forest in the foothills of the Coast Range of mountains at an elevation of 1300 m. The trail is only 600 m long and provides easy access to Agnes Lake. Travel time is about 15 minutes by foot. There are no facilities at the lake. The trail and surrounding area was heavily impacted by wildfire in 2017, but the trail has been cleared and marked with trail markers in September of 2019.',
         'TATLA LAKE',
         true,
-        'SIT',
         14,
         'RDCC'
     ),
@@ -334,7 +302,6 @@ values
         'Agur Lake is a small lake West of Summerland. The shores of the lake are marshy.',
         'SUMMERLAND',
         true,
-        'RTR',
         48,
         'RDOS'
     ),
@@ -344,7 +311,6 @@ values
         'This semi-remote site is located on the northern shores of beautiful Ahbau Lake, near the border of the Prince George and Quesnel Forest Districts. This site has camping pads to accommodate 10 larger units as well as another 10 smaller sites on the upper loop road. There is access to a lake via a concrete boat launch. The lake is popular for kayaking and boating, and offers opportunities for angling of wild fish (rainbow trout and coarse fish species). The site offers the basic amenities of fire rings, tables and pit toilets.',
         'QUESNEL',
         true,
-        'RTR',
         7,
         'RDCO'
     ),
@@ -354,7 +320,6 @@ values
         'Sand beach, boat access only, user maintained.',
         'FORT ST. JAMES',
         true,
-        'RTR',
         8,
         'RDCO'
     ),
@@ -364,7 +329,6 @@ values
         'This site is on a small fishing lake. There is 1 table and outhouse on site. Access is poor and requires a 4wd vehicle. Campers and trailers will have a difficult time getting into Aileen Lake due to water bars along the roads through cutblocks, and large water puddles and the ingrown forest for the last 800m. Best suited for the day fisherman in a pickup truck.',
         'WINFIELD',
         true,
-        'RTR',
         9,
         'RDCO'
     ),
@@ -374,7 +338,6 @@ values
         'Wilderness area, alpine hiking',
         'SLOCAN',
         true,
-        'RTR',
         10,
         'RDCK'
     ),
@@ -384,7 +347,6 @@ values
         'Water falls',
         'HOUSTON',
         true,
-        'RTR',
         11,
         'RDCK'
     ),
@@ -394,7 +356,6 @@ values
         'Are trails at the base of Idaho Peak with an elevation 2282 m / 7486 ft. The trail winds its way through forested areas.',
         'NEW DENVER',
         true,
-        'RTR',
         13,
         'RDCR'
     ),
@@ -404,7 +365,6 @@ values
         'Albright Trail',
         'Tumbler Ridge',
         true,
-        'IF',
         14,
         'RDPG'
     ),
@@ -414,7 +374,6 @@ values
         'A small shady site on an old railgrade with a short trail to the lakeshore.',
         'CAMPBELL RIVER',
         true,
-        'IF',
         14,
         'RDPG'
     ),
@@ -424,7 +383,6 @@ values
         'In conjunction with the Great Divide Trail and the Hornaday Wilderness Society, the trail was rebuilt in 2015.',
         'ELKFORD',
         true,
-        'IF',
         15,
         'RDKM'
     ),
@@ -434,7 +392,6 @@ values
         'A small, heavily treed site on the Elk River.',
         'ELKFORD',
         true,
-        'IF',
         19,
         'RDKM'
     ),
@@ -444,7 +401,6 @@ values
         'A small, partially treed site on Alexander Creek.',
         'SPARWOOD',
         true,
-        'IF',
         21,
         'RDKM'
     ),
@@ -454,7 +410,6 @@ values
         'A small, partly treed site.',
         'SPARWOOD',
         true,
-        'IF',
         98,
         'RDKM'
     ),
@@ -464,7 +419,6 @@ values
         'A small and remote lake, surrounded by trees, with a boat launch.',
         'POWELL RIVER',
         true,
-        'RTR',
         76,
         'RDSI'
     ),
@@ -474,7 +428,6 @@ values
         'A small, scenic lake with fishing opportunities.',
         'PENTICTON',
         true,
-        'RTR',
         2,
         'RDKA'
     ),
@@ -484,7 +437,6 @@ values
         'A short but steep hike through alpine meadows to the scenic Alpine Lake.',
         'TROUT CREEK',
         true,
-        'RTR',
         4,
         'RDKA'
     ),
@@ -494,7 +446,6 @@ values
         'A small mountain lake in Whistler',
         'WHISTLER',
         true,
-        'RTR',
         11,
         'RDSQ'
     ),
@@ -504,7 +455,6 @@ values
         'A parking area for the nearby Alta Lake Trail.',
         'WHISTLER',
         true,
-        'RTR',
         10,
         'RDSQ'
     ),
@@ -514,7 +464,6 @@ values
         'A trail that runs along the shore of Alta Lake, offering scenic views.',
         'WHISTLER',
         true,
-        'RTR',
         78,
         'RDSQ'
     ),
@@ -524,7 +473,6 @@ values
         'A snowmobile parking area located off Alta Lake Road.',
         'WHISTLER',
         true,
-        'RR',
         1,
         'RDSQ'
     );
