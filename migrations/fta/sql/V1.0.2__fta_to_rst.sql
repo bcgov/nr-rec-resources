0dc6c70ff3906ee5830120af7a83eb35aa1db946
marcellmueller <marcel@button.is>
2025-02-18 15:22:58 -0800

    refactor: split up fta -> rst recreation_resource insert statement

--- migrations/fta/sql/V1.0.2__fta_to_rst.sql
+++ migrations/fta/sql/V1.0.2__fta_to_rst.sql
@@ -1,60 +1,51 @@
-insert into
-    rst.recreation_resource_type_code (rec_resource_type_code, description)
-select
-    recreation_map_feature_code,
-    description
-from
-    fta.recreation_map_feature_code;
+-- Migrate data from fta schema to rst schema
 
-insert into
-    rst.recreation_resource (
-        rec_resource_id,
-        name,
-        description,
-        closest_community,
-        display_on_public_site,
-        rec_resource_type,
-        campsite_count,
-        district_code
-    )
+-- Insert data into recreation_district_code from fta.recreation_district_code
+insert into rst.recreation_resource_type_code (rec_resource_type_code, description)
+select recreation_map_feature_code, description
+from fta.recreation_map_feature_code;
+
+-- Insert data into recreation_resource from fta.recreation_project table
+insert into rst.recreation_resource (rec_resource_id, name, closest_community, display_on_public_site)
 select
-    rp.forest_file_id as rec_resource_id,
-    rp.project_name as name,
-    case
-        when rc.rec_comment_type_code = 'DESC' then rc.project_comment
-        else ''
-    end as description,
-    rp.site_location as closest_community,
-    case
-        when rp.recreation_view_ind = 'Y' then true
-        else false
-    end as display_on_public_site,
-    rmf.recreation_map_feature_code as rec_resource_type,
-    coalesce(c.campsite_count, 0) as campsite_count,
-    xref.recreation_district_code as district_code
-from
-    fta.recreation_project rp
-    left join fta.recreation_comment rc on rp.forest_file_id = rc.forest_file_id
-    left join fta.recreation_map_feature rmf on rp.forest_file_id = rmf.forest_file_id
-    left join
-    (select forest_file_id, count(*) as campsite_count
+    rp.forest_file_id,
+    rp.project_name,
+    rp.site_location,
+    case when rp.recreation_view_ind = 'Y' then true else false end
+from fta.recreation_project rp;
+
+-- Add description from fta.recreation_comment table
+update rst.recreation_resource rr
+set description = rc.project_comment
+from fta.recreation_comment rc
+where rr.rec_resource_id = rc.forest_file_id
+and rc.rec_comment_type_code = 'DESC';
+
+-- Add campsite_count from recreation_defined_campsite
+update rst.recreation_resource rr
+set campsite_count = c.campsite_count
+from (
+    select forest_file_id, count(*) as campsite_count
     from fta.recreation_defined_campsite
-    group by forest_file_id) c on rp.forest_file_id = c.forest_file_id
-    left join fta.recreation_district_xref xref on rp.forest_file_id = xref.forest_file_id
-on conflict (rec_resource_id) do nothing;
+    group by forest_file_id
+) c
+where rr.rec_resource_id = c.forest_file_id;
 
+-- Add district_code from recreation_district_xref
+update rst.recreation_resource rr
+set district_code = xref.recreation_district_code
+from fta.recreation_district_xref xref
+where rr.rec_resource_id = xref.forest_file_id;
 
-insert into
-    rst.recreation_activity (rec_resource_id, recreation_activity_code)
+-- Insert data into recreation_activity from fta.recreation_activity
+insert into rst.recreation_activity (rec_resource_id, recreation_activity_code)
 select
     ra.forest_file_id as rec_resource_id,
     -- Convert strings codes ie '01', '02' to integers
     cast(ra.recreation_activity_code as int) as recreation_activity_code
-from
-    fta.recreation_activity ra;
+from fta.recreation_activity ra;
 
-insert into
-    rst.recreation_status (rec_resource_id, status_code, comment)
+insert into rst.recreation_status (rec_resource_id, status_code, comment)
 select
     forest_file_id,
     case
@@ -62,7 +53,5 @@ select
         else 1 -- Open
     end as recreation_status_code,
     project_comment as description
-from
-    fta.recreation_comment
-where
-    rec_comment_type_code = 'CLOS';
+from fta.recreation_comment
+where rec_comment_type_code = 'CLOS';
