ff232bb7b2f858286abb720277ab5629c5c7492a
marcellmueller <marcel@button.is>
2025-02-19 08:46:50 -0800

    chore: update recreation resource type insert statement

--- migrations/fta/sql/V1.0.2__fta_to_rst.sql
+++ migrations/fta/sql/V1.0.2__fta_to_rst.sql
@@ -58,9 +58,12 @@ select recreation_map_feature_code, description
 from fta.recreation_map_feature_code;
 
 -- Insert into recreation_resource_type from fta.recreation_map_feature
+-- Select distinct, ordered by amend_status_date as there are some duplicated with current_ind = 'Y'
 insert into rst.recreation_resource_type (rec_resource_id, rec_resource_type_code)
 select distinct on (rmf.forest_file_id)
     rmf.forest_file_id,
     rmf.recreation_map_feature_code
 from fta.recreation_map_feature rmf
-order by rmf.forest_file_id, rmf.amend_status_date desc limit 1;
+where rmf.forest_file_id in (select rec_resource_id from rst.recreation_resource)
+and rmf.current_ind = 'Y'
+order by rmf.forest_file_id, rmf.amend_status_date desc;
