4ee7646ad1980bffbb076220fa67fa9a494f5a8a
marcellmueller <marcel@button.is>
2025-02-20 13:58:44 -0800

    chore: undo reverse access code columns

--- migrations/fta/sql/V1.0.2__fta_to_rst.sql
+++ migrations/fta/sql/V1.0.2__fta_to_rst.sql
@@ -143,9 +143,8 @@ insert into
     rst.recreation_access (rec_resource_id, access_code, sub_access_code)
 select
     ra.forest_file_id as rec_resource_id,
-    -- access_code and sub_access_code are reversed in the fta data for some reason
-    ra.recreation_sub_access_code as access_code,
-    ra.recreation_access_code as sub_access_code
+    ra.recreation_access_code as access_code,
+    ra.recreation_sub_access_code as sub_access_code
 from
     fta.recreation_access ra;
 
