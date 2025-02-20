27b8dafaccaf40e679df61c9b5feacf465b7c1ae
marcellmueller <marcel@button.is>
2025-02-20 11:29:34 -0800

    chore: add recreation sub access code table

--- migrations/rst/sql/V1.0.5__recreation_access.sql
+++ migrations/rst/sql/V1.0.5__recreation_access.sql
@@ -1,4 +1,4 @@
-create table rst.recreation_access_code (
+create table if not exists rst.recreation_access_code (
     access_code varchar(3) primary key,
     description varchar(120),
     sub_description varchar(120)
@@ -12,10 +12,22 @@ comment on column rst.recreation_access_code.description is 'Description of the 
 
 comment on column rst.recreation_access_code.sub_description is 'Additional description of the code value';
 
-create table rst.recreation_access (
+create table if not exists rst.recreation_sub_access_code (
+    sub_access_code varchar(3) primary key,
+    description varchar(120)
+);
+
+comment on table rst.recreation_sub_access_code is 'Codes describing the Recreation Sub Access types within a project.';
+
+comment on column rst.recreation_sub_access_code.sub_access_code is 'Code describing the Recreation Sub Access types.';
+
+comment on column rst.recreation_sub_access_code.description is 'Description of the code value.';
+
+create table if not exists rst.recreation_access (
     id serial primary key, -- This is a surrogate key to make Prisma happy
     rec_resource_id varchar(20) not null references rst.recreation_resource (rec_resource_id),
-    access_code varchar(3) not null references rst.recreation_access_code (access_code)
+    access_code varchar(3) not null references rst.recreation_access_code (access_code),
+    sub_access_code varchar(3) references rst.recreation_sub_access_code (sub_access_code)
 );
 
 comment on table rst.recreation_access is 'Recreation Resource Access types';
@@ -24,6 +36,7 @@ comment on column rst.recreation_access.rec_resource_id is 'Identification manua
 
 comment on column rst.recreation_access.access_code is 'Code identifying the type of access';
 
+-- Manually inserting these to split the description with sub_description
 insert into
     rst.recreation_access_code (access_code, description, sub_description)
 values
