e3530cb85572aed215dd84445554e89002c19d63
marcellmueller <marcel@button.is>
2025-02-24 10:18:24 -0800

    chore: fix rebase issues

--- frontend/src/service/recreation-resource/models/index.ts
+++ frontend/src/service/recreation-resource/models/index.ts
@@ -7,5 +7,7 @@ export * from './HealthControllerCheck200ResponseInfoValue';
 export * from './HealthControllerCheck503Response';
 export * from './PaginatedRecreationResourceDto';
 export * from './RecreationActivityDto';
+export * from './RecreationCampsiteDto';
+export * from './RecreationFeeDto';
 export * from './RecreationResourceDto';
 export * from './RecreationStatusDto';
