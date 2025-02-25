7854d65ec86532ef714c53d0e2f341a95494d321
marcellmueller <marcel@button.is>
2025-02-25 14:01:47 -0800

    chore: test temporal tables

--- migrations/rst/sql/V1.0.2__util_functions.sql
+++ migrations/rst/sql/V1.0.2__util_functions.sql
@@ -7,3 +7,28 @@ begin
     execute format('alter table %I.%I add column if not exists updated_by text', schema_name, table_name);
 end;
 $$ language plpgsql;
+
+
+
+create or replace function setup_temporal_table(schema_name text, table_name text)
+returns void language plpgsql as $$
+declare
+    trigger_name text;
+    versioning_function text;
+begin
+    trigger_name := table_name || '_versioning_trigger';
+    versioning_function := 'versioning';
+
+    -- add the system period column
+    execute format('alter table %I.%I add column sys_period tstzrange not null default tstzrange(current_timestamp, null);', schema_name, table_name);
+
+    -- create the history table
+    execute format('create table %I.%I (like %I.%I excluding constraints);', schema_name, table_name || '_history', schema_name, table_name);
+
+    -- create the trigger
+    execute format('create trigger %I before insert or update or delete on %I.%I for each row execute procedure %I(''sys_period'', %L, true);',
+                   schema_name || table_name || '_versioning_trigger', schema_name, table_name, versioning_function, schema_name || '.' || table_name || '_history');
+
+    raise notice 'temporal tables setup completed for % and history table %', schema_name || '.' || table_name, schema_name || '.' || table_name || '_history';
+end;
+$$;
