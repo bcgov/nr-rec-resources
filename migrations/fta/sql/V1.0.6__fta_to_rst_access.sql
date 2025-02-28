00c86f9652abc6b2a52eab6372a49600c957a30e
marcellmueller <marcel@button.is>
2025-02-28 10:44:36 -0800

    chore: update access fta -> rst insert statement to account for duplicate records

--- migrations/fta/sql/V1.0.6__fta_to_rst_access.sql
+++ migrations/fta/sql/V1.0.6__fta_to_rst_access.sql
@@ -8,9 +8,14 @@ from
 
 insert into
     rst.recreation_access (rec_resource_id, access_code, sub_access_code)
-select
+select distinct -- distinct is used to avoid duplicate records that exist
     ra.forest_file_id as rec_resource_id,
     ra.recreation_access_code as access_code,
     ra.recreation_sub_access_code as sub_access_code
 from
-    fta.recreation_access ra;
+    fta.recreation_access ra
+on conflict (rec_resource_id, access_code, sub_access_code)
+do update
+set
+    access_code = excluded.access_code,
+    sub_access_code = excluded.sub_access_code;
