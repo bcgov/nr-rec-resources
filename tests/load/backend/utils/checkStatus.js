d0e0f9b32f751ff45752950ce41e866ceb3525ca
marcellmueller <marcel@button.is>
2025-03-07 16:03:39 -0800

    test: add k6 recreation-resource search tests

---
+++ tests/load/backend/utils/checkStatus.js
@@ -0,0 +1,21 @@
+import { check } from "k6";
+import { Rate } from "k6/metrics";
+export const errorRate = new Rate("errors");
+
+const checkStatus = (response, checkName, statusCode = 200) => {
+  const success = check(response, {
+    [checkName]: (r) => {
+      if (r.status === statusCode) {
+        return true;
+      } else {
+        console.error(
+          checkName + " failed. Incorrect response code." + r.status,
+        );
+        return false;
+      }
+    },
+  });
+  errorRate.add(!success, { tag1: checkName });
+};
+
+export default checkStatus;
