512a5ad4b07778f51857989376172b63b9edd814
marcellmueller <marcel@button.is>
2025-03-11 15:03:53 -0700

    test: add playwright pom for filters

--- frontend/e2e/constants.ts
+++ frontend/e2e/constants.ts
@@ -1,1 +1,5 @@
+import { MAX_VISIBLE_OPTIONS } from '@/components/search/filters/FilterGroup';
+
 export const BASE_URL = process.env.E2E_BASE_URL || 'http://localhost:3000';
+
+export const MAX_VISIBLE_FILTERS = MAX_VISIBLE_OPTIONS;
---
+++ frontend/e2e/poms/filter.ts
@@ -0,0 +1,139 @@
+// https://playwright.dev/docs/pom
+// Page object models (POM) simplify authoring by creating a higher-level API which suits your application &
+// simplify maintenance by capturing element selectors in one place and create reusable code to avoid repetition.
+
+import { expect, Locator, Page } from '@playwright/test';
+import { MAX_VISIBLE_FILTERS } from 'e2e/constants';
+import {
+  accessTypeFilterOptions,
+  districtFilterOptions,
+  facilitiesFilterOptions,
+  thingsToDoFilterOptions,
+  typeFilterOptions,
+} from 'e2e/data/filters';
+import { FilterGroup } from 'e2e/enum/filter';
+
+export class FilterPOM {
+  readonly page: Page;
+
+  readonly districtFilters: Locator;
+
+  readonly typeFilters: Locator;
+
+  readonly thingsToDoFilters: Locator;
+
+  readonly facilitiesFilters: Locator;
+
+  readonly accessTypeFilters: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+
+    this.districtFilters = page
+      .locator('fieldset')
+      .filter({ hasText: FilterGroup.DISTRICT });
+
+    this.typeFilters = page
+      .locator('fieldset')
+      .filter({ hasText: FilterGroup.TYPE });
+
+    this.thingsToDoFilters = page
+      .locator('fieldset')
+      .filter({ hasText: FilterGroup.THINGS_TO_DO });
+
+    this.facilitiesFilters = page
+      .locator('fieldset')
+      .filter({ hasText: FilterGroup.FACILITIES });
+
+    this.accessTypeFilters = page
+      .locator('fieldset')
+      .filter({ hasText: FilterGroup.ACCESS_TYPE });
+  }
+
+  async clickShowLessFilters(filterGroup: Locator) {
+    await filterGroup.getByRole('button', { name: /Show less/ }).click();
+  }
+
+  async clickShowAllFilters(filterGroup: Locator) {
+    await filterGroup.getByRole('button', { name: /Show all/ }).click();
+  }
+
+  async verifyFilterGroup(
+    filterGroup: Locator,
+    filterOptions: { label: string; count: number }[],
+  ) {
+    const showLessButton = filterGroup.getByRole('button', {
+      name: /Show less/,
+    });
+    const visibleFilters =
+      (await showLessButton.count()) > 0
+        ? filterOptions
+        : filterOptions.slice(0, MAX_VISIBLE_FILTERS);
+
+    for (const filter of visibleFilters) {
+      const { label, count } = filter;
+      expect(filterGroup.getByLabel(`${label} (${count})`)).toBeVisible();
+    }
+  }
+
+  async verifyDistrictFilterGroup() {
+    await this.verifyFilterGroup(this.districtFilters, districtFilterOptions);
+  }
+
+  async verifyTypeFilterGroup() {
+    await this.verifyFilterGroup(this.typeFilters, typeFilterOptions);
+  }
+
+  async verifyThingsToDoFilterGroup() {
+    await this.verifyFilterGroup(
+      this.thingsToDoFilters,
+      thingsToDoFilterOptions,
+    );
+  }
+
+  async verifyFacilitiesFilterGroup() {
+    await this.verifyFilterGroup(
+      this.facilitiesFilters,
+      facilitiesFilterOptions,
+    );
+  }
+
+  async verifyAccessTypeFilterGroup() {
+    await this.verifyFilterGroup(
+      this.accessTypeFilters,
+      accessTypeFilterOptions,
+    );
+  }
+
+  async verifyInitialFilterMenu() {
+    await this.verifyDistrictFilterGroup();
+    await this.verifyTypeFilterGroup();
+    await this.verifyThingsToDoFilterGroup();
+    await this.verifyFacilitiesFilterGroup();
+    await this.verifyAccessTypeFilterGroup();
+  }
+
+  async toggleFilterOn(filterGroup: Locator, filterOption: string) {
+    expect(
+      filterGroup.getByRole('checkbox', { name: filterOption }),
+    ).not.toBeChecked();
+    await filterGroup
+      .getByRole('checkbox', { name: filterOption })
+      .check({ force: true });
+    expect(
+      filterGroup.getByRole('checkbox', { name: filterOption }),
+    ).toBeChecked();
+  }
+
+  async toggleFilterOff(filterGroup: Locator, filterOption: string) {
+    expect(
+      filterGroup.getByRole('checkbox', { name: filterOption }),
+    ).toBeChecked();
+    await filterGroup
+      .getByRole('checkbox', { name: filterOption })
+      .uncheck({ force: true });
+    expect(
+      filterGroup.getByRole('checkbox', { name: filterOption }),
+    ).not.toBeChecked();
+  }
+}
