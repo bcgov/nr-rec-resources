6f21de461493fb1dcb908f89da87de14819666ea
Hamid Habib <hamidhabibb@gmail.com>
2025-03-14 13:30:41 -0400

    feat(recreation): added recreation maintainance

--- migrations/rst/sql/V1.1.13__recreation_maintainace.sql
+++ migrations/rst/sql/V1.1.13__recreation_maintainace.sql
@@ -1,11 +1,12 @@
-select upsert_timestamp_columns('rst', 'recreation_maintainace_code', true);
-
-select setup_temporal_table('rst', 'recreation_maintainace_code');
-
 create table if not exists rst.recreation_maintainace_code (
     recreation_maintainace_code varchar(1) primary key,
     description varchar(120) not null
 );
+
+select upsert_timestamp_columns('rst', 'recreation_maintainace_code', true);
+
+select setup_temporal_table('rst', 'recreation_maintainace_code');
+
 
 comment on table rst.recreation_maintainace_code is 'Recreation maintenance standard codes for classifying site maintenance levels.';
 comment on column rst.recreation_maintainace_code.recreation_maintainace_code is 'A code indicating a specific maintenance standard. E.g., M for Maintained to Standard, U for User Maintained.';
@@ -16,12 +17,12 @@ values
     ('M', 'Maintained to Standard'),
     ('U', 'User Maintained');
 
-select upsert_timestamp_columns('rst', 'recreation_maintainace', true);
-
-select setup_temporal_table('rst', 'recreation_maintainace');
-
 create table if not exists rst.recreation_maintainace (
     id serial primary key,
     rec_resource_id varchar(200) not null references rst.recreation_resource (rec_resource_id),
     recreation_maintainace_code varchar(1) not null references rst.recreation_maintainace_code (recreation_maintainace_code)
 );
+
+select upsert_timestamp_columns('rst', 'recreation_maintainace', true);
+
+select setup_temporal_table('rst', 'recreation_maintainace');
