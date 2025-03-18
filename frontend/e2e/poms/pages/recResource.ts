24a415e7b7d70dff405cb9818e4d11a3ae77429a
marcellmueller <marcel@button.is>
2025-03-18 14:47:55 -0700

    test: update e2e tests due to rebase changes

--- frontend/e2e/poms/pages/recResource.ts
+++ frontend/e2e/poms/pages/recResource.ts
@@ -14,7 +14,7 @@ export class RecreationResourcePOM {
     this.page = page;
   }
 
-  async route(resourceId: string = 'REC204117') {
+  async route(resourceId: string = 'REC160432') {
     await this.page.goto(`${this.baseUrl}/${resourceId}`);
     await waitForImagesToLoad(this.page);
   }
@@ -62,6 +62,5 @@ export class RecreationResourcePOM {
         canvas.remove();
       }
     }, MAP_CANVAS_SELECTOR);
-
   }
 }
