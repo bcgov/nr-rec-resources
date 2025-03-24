fa9d9baf189cdeb74bfd60504d954a77d535dc44
marcellmueller <marcel@button.is>
2025-03-24 16:17:50 -0700

    chore: remove recreation_resource_type table

--- migrations/rst/sql/V1.0.4__recreation_resource_type.sql
+++ migrations/rst/sql/V1.0.4__recreation_resource_type.sql
@@ -12,25 +12,3 @@ comment on table rst.recreation_resource_type_code is 'Codes describing types of
 comment on column rst.recreation_resource_type_code.rec_resource_type_code is 'Identifies the Recreation Resource Type Code';
 
 comment on column rst.recreation_resource_type_code.description is 'Description of the code value';
-
-
--- Based on fta.recreation_map_feature, though that table allows for multiple types per project we
--- are only going to insert the latest one sorted by amend_status_date
--- For the admin app we still need to decide on how to handle historical data - keep in same table or move to history tracking table
-create table if not exists rst.recreation_resource_type (
-    rec_resource_id varchar(20) primary key references rst.recreation_resource (rec_resource_id),
-    rec_resource_type_code varchar(10) not null references rst.recreation_resource_type_code (rec_resource_type_code),
-    unique (rec_resource_id, rec_resource_type_code)
-);
-
-select upsert_timestamp_columns('rst', 'recreation_resource_type');
-
-select setup_temporal_table('rst', 'recreation_resource_type');
-
-create index idx_recreation_resource_type_rec_resource_id on rst.recreation_resource_type(rec_resource_id);
-
-comment on table rst.recreation_resource_type is 'Captures both current and historical attributes for Recreation Map Types';
-
-comment on column rst.recreation_resource_type.rec_resource_id is 'Identification manually assigned to a Recreation Resource';
-
-comment on column rst.recreation_resource_type.rec_resource_type_code is 'Identifies the Recreation Resource Type Code';
