cd8baab643bc0ac84476f111ea22b484ba42abc7
marcellmueller <marcel@button.is>
2025-03-24 16:45:37 -0700

    chore: fix structure counts

--- backend/src/recreation-resource/service/search.service.ts
+++ backend/src/recreation-resource/service/search.service.ts
@@ -53,45 +53,49 @@ export class RecreationResourceSearchService {
     const [recreationResources, combinedRecordCounts, combinedStaticCounts] =
       await this.prisma.$transaction([
         this.prisma.$queryRaw<any[]>`
-          select * from recreation_resource_search_view
-          ${whereClause}
-          order by name asc
-          limit ${take}
-          ${skip ? Prisma.sql`OFFSET ${skip}` : Prisma.sql``};`,
+        select * from recreation_resource_search_view
+        ${whereClause}
+        order by name asc
+        limit ${take}
+        ${skip ? Prisma.sql`OFFSET ${skip}` : Prisma.sql``};`,
+
         this.prisma.$queryRaw<any[]>`
-          with filtered_resources as (
-              select rec_resource_id, has_toilets, has_tables
-              from recreation_resource_search_view
-              ${whereClause}
-          )
+        with filtered_resources as (
+          select rec_resource_id, has_toilets, has_tables
+          from recreation_resource_search_view
+          ${whereClause}
+        ),
+        structure_counts as (
           select
-            rac.recreation_activity_code,
-            rac.description,
-            cast(count(distinct case when fra.rec_resource_id is not null then fra.rec_resource_id end) as int) as recreation_activity_count,
-            cast(count(distinct case when fr.has_toilets = true then fr.rec_resource_id end) as int) as total_toilet_count,
-            cast(count(distinct case when fr.has_tables = true then fr.rec_resource_id end) as int) as total_table_count,
-            cast((select count(*) from filtered_resources) as int) as total_count
-          from rst.recreation_activity_code rac
-            left join rst.recreation_activity ra on ra.recreation_activity_code = rac.recreation_activity_code
-            left join filtered_resources fra on fra.rec_resource_id = ra.rec_resource_id
-            left join filtered_resources fr on fr.rec_resource_id = ra.rec_resource_id
-          where rac.recreation_activity_code not in (${Prisma.join(EXCLUDED_ACTIVITY_CODES)})
-          group by
-              rac.recreation_activity_code,
-              rac.description;`,
+            cast(count(case when has_toilets = true then rec_resource_id end) as int) as total_toilet_count,
+            cast(count(case when has_tables = true then rec_resource_id end) as int) as total_table_count
+          from filtered_resources
+        )
+        select
+          rac.recreation_activity_code,
+          rac.description,
+          cast(count(distinct case when fra.rec_resource_id is not null then fra.rec_resource_id end) as int) as recreation_activity_count,
+          (select total_toilet_count from structure_counts) as total_toilet_count,
+          (select total_table_count from structure_counts) as total_table_count,
+          cast((select count(*) from filtered_resources) as int) as total_count
+        from rst.recreation_activity_code rac
+          left join rst.recreation_activity ra on ra.recreation_activity_code = rac.recreation_activity_code
+          left join filtered_resources fra on fra.rec_resource_id = ra.rec_resource_id
+        where rac.recreation_activity_code not in (${Prisma.join(EXCLUDED_ACTIVITY_CODES)})
+        group by rac.recreation_activity_code, rac.description;`,
         this.prisma.$queryRaw<any[]>`
           (select 'district' as type, district_code as code, description, cast(resource_count as integer) as count
-            from recreation_resource_district_count_view
-            where district_code not in (${Prisma.join(EXCLUDED_RECREATION_DISTRICTS)}))
-          union all
-          (select 'access' as type, access_code as code, access_description as description, cast(count as integer) as count
-            from recreation_resource_access_count_view
-            where access_code not in (${Prisma.join(EXCLUDED_RESOURCE_TYPES)}))
-          union all
-          (select 'type' as type, rec_resource_type_code as code, description, cast(count as integer) as count
-            from recreation_resource_type_count_view
-            where rec_resource_type_code not in (${Prisma.join(EXCLUDED_RESOURCE_TYPES)}))
-          order by description asc;`,
+          from recreation_resource_district_count_view
+          where district_code not in (${Prisma.join(EXCLUDED_RECREATION_DISTRICTS)}))
+        union all
+      (select 'access' as type, access_code as code, access_description as description, cast(count as integer) as count
+          from recreation_resource_access_count_view
+          where access_code not in (${Prisma.join(EXCLUDED_RESOURCE_TYPES)}))
+        union all
+      (select 'type' as type, rec_resource_type_code as code, description, cast(count as integer) as count
+          from recreation_resource_type_count_view
+          where rec_resource_type_code not in (${Prisma.join(EXCLUDED_RESOURCE_TYPES)}))
+        order by description asc; `,
       ]);
 
     return {
