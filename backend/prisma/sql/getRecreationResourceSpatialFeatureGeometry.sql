79216e20d660b0f1a671728ee322f44fd7a1d5ff
Jimmy Palelil <30486617+jimmypalelil@users.noreply.github.com>
2025-03-24 08:51:59 -0700

    fix: use active geospatial data for mapping rec sites and trails (#481)
    Co-authored-by: jimmypalelil <jimmy.palelil@gov.bc.ca>

--- backend/prisma/sql/getRecreationResourceSpatialFeatureGeometry.sql
+++ backend/prisma/sql/getRecreationResourceSpatialFeatureGeometry.sql
@@ -1,6 +1,7 @@
 -- Gets the spatial features for a rec resource in GeoJSON format
 SELECT public.st_asgeojson(rmfg.geometry) as spatial_feature_geometry
 from rst.recreation_map_feature rmf
-         join rst.recreation_map_feature_geom rmfg
-              on rmf.rmf_skey = rmfg.rmf_skey
+         inner join rst.recreation_map_feature_geom rmfg
+                    using (rmf_skey)
 where rmf.rec_resource_id = $1
+  and rmf.retirement_date is null; -- only show active geospatial data
--- backend/src/prisma-generated-sql/getRecreationResourceSpatialFeatureGeometry.js
+++ backend/src/prisma-generated-sql/getRecreationResourceSpatialFeatureGeometry.js
@@ -3,5 +3,5 @@ const {
   makeTypedQueryFactory: $mkFactory,
 } = require("@prisma/client/runtime/library");
 exports.getRecreationResourceSpatialFeatureGeometry = /*#__PURE__*/ $mkFactory(
-  "SELECT public.st_asgeojson(rmfg.geometry) as spatial_feature_geometry\nfrom rst.recreation_map_feature rmf\njoin rst.recreation_map_feature_geom rmfg\non rmf.rmf_skey = rmfg.rmf_skey\nwhere rmf.rec_resource_id = $1",
+  "SELECT public.st_asgeojson(rmfg.geometry) as spatial_feature_geometry\nfrom rst.recreation_map_feature rmf\ninner join rst.recreation_map_feature_geom rmfg\nusing (rmf_skey)\nwhere rmf.rec_resource_id = $1\nand rmf.retirement_date is null; -- only show active geospatial data",
 );
