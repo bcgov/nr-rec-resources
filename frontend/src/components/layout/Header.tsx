10a429fea443b528e5a4c64cb1cae1286f77f78d
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-04-08 17:18:57 -0700

    feat(header-nav): added link to interactive map

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -3,6 +3,10 @@ import RSTLogo from '@/images/RST_nav_logo.svg';
 import '@/components/layout/Header.scss';
 import BetaBanner from './BetaBanner';
 import { ROUTE_PATHS } from '@/routes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
+import { Stack } from 'react-bootstrap';
+import { EXTERNAL_LINKS } from '@/data/urls';
 
 const Header = () => {
   return (
@@ -35,8 +39,19 @@ const Header = () => {
           aria-label="Secondary header site navigation"
           className="header-nav sub"
         >
-          <a href="/search">Find a site or trail</a>
-          <a href="/">Contact</a>
+          <Stack direction="horizontal" gap={3}>
+            <a href="/search">Find a site or trail</a>
+            <a
+              target="_blank"
+              href={EXTERNAL_LINKS.RST_ARCGIS_MAP_FULL_SCREEN}
+              rel="noreferrer"
+            >
+              <Stack direction={'horizontal'} gap={1}>
+                Interactive map
+                <FontAwesomeIcon icon={faExternalLink} />
+              </Stack>
+            </a>
+          </Stack>
         </nav>
       </div>
     </header>
