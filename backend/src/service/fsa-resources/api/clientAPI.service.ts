0e9f5bcb7156e5986c17bc6d43c30841c8a74da1
Felipe Barreta <fbarreta@gmail.com>
2025-04-15 10:18:07 -0700

    chore: access api url on env file

--- backend/src/service/fsa-resources/api/clientAPI.service.ts
+++ backend/src/service/fsa-resources/api/clientAPI.service.ts
@@ -24,7 +24,7 @@ import { COLLECTION_FORMATS } from "../variables";
 
 @Injectable()
 export class ClientAPIService {
-  protected basePath = "https://nr-forest-client-api-prod.api.gov.bc.ca";
+  protected basePath = process.env.API_URL;
   public defaultHeaders: Record<string, string> = {};
   public configuration = new Configuration();
   protected httpClient: HttpService;
