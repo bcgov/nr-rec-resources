6779b5ce67f560e7ccc8e2bc314389c564068435
marcellmueller <marcel@button.is>
2025-04-17 14:57:38 -0700

    chore: exclude gathering and hunting activities from filter menu

--- backend/src/recreation-resource/constants/service.constants.ts
+++ backend/src/recreation-resource/constants/service.constants.ts
@@ -1,4 +1,4 @@
-export const EXCLUDED_ACTIVITY_CODES = [26];
+export const EXCLUDED_ACTIVITY_CODES = [10, 18, 26];
 export const EXCLUDED_RECREATION_DISTRICTS = ["RDQC", "RDRM"];
 export const EXCLUDED_RESOURCE_TYPES = ["RR"];
 export const OPEN_STATUS = {
