7cf8320c2675c69cbbcfd3cd43ef7dee7e8c9594
marcellmueller <marcel@button.is>
2025-04-24 11:38:40 -0700

    chore: add use matomo tracking

--- frontend/src/hooks/useMatomo.ts
+++ frontend/src/hooks/useMatomo.ts
@@ -1,3 +1,23 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const useMatomoTracking = () => {
+  // TODO: figure out if this is necessary
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window._paq) {
+      console.log('Tracking page view:', {
+        url: window.location.href,
+        title: document.title,
+      });
+      window._paq.push(['setCustomUrl', window.location.href]);
+      window._paq.push(['setDocumentTitle', document.title]);
+      window._paq.push(['trackPageView']);
+    }
+  }, [location]);
+};
+
 export const useMatomo = () => {
   const trackEvent = ({
     category,
