4c54cb145bc71da15bc13aef97f897989cb0ce41
Felipe Barreta <fbarreta@gmail.com>
2025-04-29 14:47:43 -0700

    chore: added aria label and group role to search

--- frontend/src/components/recreation-search-form/RecreationSearchForm.tsx
+++ frontend/src/components/recreation-search-form/RecreationSearchForm.tsx
@@ -53,7 +53,7 @@ export const RecreationSearchForm: FC<RecreationSearchFormProps> = ({
     >
       <Row className="gy-3 gx-0 gx-lg-3">
         <Col md={12} lg="auto" className="flex-grow-0 flex-lg-grow-1">
-          <InputGroup className="search-input-group">
+          <InputGroup className="search-input-group" role="group" aria-labelledby={placeholder}>
             <FormControl
               type="text"
               value={inputValue}
