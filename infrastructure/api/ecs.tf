a35747baf58ea7db74eaa9b5c0787da88ba47739
marcellmueller <marcel@button.is>
2025-05-01 13:00:04 -0700

    fix: migration issues

--- infrastructure/api/ecs.tf
+++ infrastructure/api/ecs.tf
@@ -85,10 +85,9 @@ module "flyway_task" {
         value = "true"
       },
       {
-        # This defaults to true, though we want to enable it only in dev to reset the database
         # also needs an update in migrations/rst/entrypoint.sh file for the flyaway ecs task to run correctly
         name = "FLYWAY_CLEAN_DISABLED"
-        value = contains(["dev"], local.rds_app_env) ? "false" : "true"
+        value = "true"
       }
   ]
   aws_region      = var.aws_region
