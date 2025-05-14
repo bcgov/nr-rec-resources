d10eee2798dfffcb27fb1c2c840a7f3d4acd688e
Felipe Barreta <fbarreta@gmail.com>
2025-05-14 10:22:06 -0700

    chore: remove prisma client test

--- admin/backend/src/app.module.ts
+++ admin/backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { TestController } from "./test.controller";
-import { TestService } from "./test.service";
 
 @Module({
   imports: [],
-  controllers: [AppController, TestController],
-  providers: [AppService, TestService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
