c4401d404082d5791e1d88caad32936c5db07526
marcellmueller <marcel@button.is>
2025-05-15 11:30:47 -0700

    chore: text updates

--- public/frontend/src/components/landing-page/LandingPage.tsx
+++ public/frontend/src/components/landing-page/LandingPage.tsx
@@ -1,8 +1,6 @@
 import { FC } from 'react';
 import { Button, Stack } from 'react-bootstrap';
 import './LandingPage.scss';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { trackClickEvent } from '@/utils/matomo';
 import { EXTERNAL_LINKS } from '@/data/urls';
 import {
@@ -16,22 +14,18 @@ import {
 } from '@/components/landing-page/constants';
 
 const LearnMoreLink: FC = () => (
-  <div className="learn-more-link">
-    <a
-      href={EXTERNAL_LINKS.RST_GOV_BC_INFO}
-      rel="noopener noreferrer"
-      onClick={trackClickEvent({
-        category: 'Learn More',
-        name: 'Learn more about Recreation Sites and Trails BC',
-      })}
-      aria-label="Learn more about Recreation Sites and Trails BC"
-    >
-      <Stack direction="horizontal" gap={1} className="align-items-center">
-        Learn more about Recreation Sites and Trails BC{' '}
-        <FontAwesomeIcon icon={faChevronRight} aria-hidden={true} />
-      </Stack>
-    </a>
-  </div>
+  <Button
+    variant="primary"
+    href={EXTERNAL_LINKS.RST_GOV_BC_INFO}
+    rel="noopener noreferrer"
+    onClick={trackClickEvent({
+      category: 'Learn More',
+      name: 'Learn more about us',
+    })}
+    aria-label="Learn more about us"
+  >
+    Learn more about us
+  </Button>
 );
 
 const FeedbackButton: FC = () => (
@@ -56,45 +50,33 @@ export const LandingPage: FC = () => {
         gap={5}
       >
         <ContentSection
-          aria-label="Welcome to the new Recreation Sites and Trails BC beta site!"
+          aria-label="Welcome to the new Recreation Sites and Trails BC beta site"
           headingComponent={
-            <h1>
-              Welcome to the new Recreation Sites and Trails BC beta site!
-            </h1>
+            <h1>Welcome to the new Recreation Sites and Trails BC beta site</h1>
           }
           sectionContent={
             <>
               <p>
-                We're building a new site to help you better learn about, visit,
-                and enjoy the recreation sites and trails we all care for
-                deeply.
+                We're launching a new site to help you discover, explore, and
+                enjoy the recreation areas and trails we all cherish.
               </p>
               <p>
-                This new beta version of{' '}
-                <a
-                  href="https://www.sitesandtrailsbc.ca/"
-                  target="_blank"
-                  aria-label="Link to the current sites and trails BC web site"
-                  rel="noreferrer noopener"
-                  className="research-signup-link"
-                >
-                  sitesandtrailsbc.ca
-                </a>{' '}
-                is just the beginning. We'll continue improving the site based
-                on research with the people who use it. It will be an evolving,
-                collaborative work in progress.
+                This beta version is just the beginning. We're committed to
+                improving it through ongoing research and feedback from people
+                like you. It’s a collaborative, evolving project—and we’d love
+                for you to be part of the journey.
               </p>
               <p>
-                Share in the adventure. We'd love for you to{' '}
                 <a
                   rel="noreferrer noopener"
                   className="research-signup-link"
                   href={EXTERNAL_LINKS.RESEARCH_PARTICIPANT_SIGN_UP}
                 >
-                  join us
-                </a>
-                .
+                  Sign up
+                </a>{' '}
+                to join our design research and help shape what comes next.
               </p>
+              <FeedbackButton />
             </>
           }
           imageBasePath={LANDING_PAGE_IMAGE_BASE_PATHS.WELCOME}
@@ -111,11 +93,14 @@ export const LandingPage: FC = () => {
           sectionContent={
             <>
               <p>
-                Our digital services will be simple, streamlined, and
-                human-centred. We'll make improvements based on real-world user
-                experience research and testing. Are you interested in being
-                part of improving our services? Please share your feedback on
-                the new site.
+                Our digital services are designed to be simple, streamlined, and
+                centered around real people.
+              </p>
+              <p>
+                We’re continuously improving them through real-world research
+                and user testing. Want to help shape the future of these
+                services? Share your feedback on the new site-we’d love to hear
+                from you.
               </p>
               <FeedbackButton />
             </>
@@ -139,11 +124,19 @@ export const LandingPage: FC = () => {
                 you to find recreation sites and trails near you, and on the way
                 to wherever you are headed.
               </p>
-              <p className="mb-0">
-                In the meantime, you can still use our original interactive map
+              <p>
+                In the meantime, you can still use our{' '}
+                <a
+                  rel="noreferrer noopener"
+                  className="research-signup-link"
+                  href={EXTERNAL_LINKS.RESEARCH_PARTICIPANT_SIGN_UP}
+                >
+                  original interactive map
+                </a>{' '}
                 to explore places to go, road conditions, and important wildfire
                 information.
               </p>
+              <FeedbackButton />
             </>
           }
           imageBasePath={LANDING_PAGE_IMAGE_BASE_PATHS.MAP}
