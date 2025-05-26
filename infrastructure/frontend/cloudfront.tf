210e275523634a459d22e7863acefb22066a6e67
marcellmueller <marcel@button.is>
2025-05-26 13:56:10 -0700

    chore: test deployment

--- infrastructure/frontend/cloudfront.tf
+++ infrastructure/frontend/cloudfront.tf
@@ -53,10 +53,10 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
   name = "${var.app_name}-security-headers"
 
   security_headers_config {
-    content_security_policy {
-      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
-      override                = true
-    }
+    # content_security_policy {
+    #   content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+    #   override                = true
+    # }
     content_type_options {
       override = true
     }
