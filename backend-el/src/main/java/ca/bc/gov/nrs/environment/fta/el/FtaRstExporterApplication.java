8ed6f5183cc3db71640814ed65cc019cd1d850e4
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-06-03 17:24:25 -0700

    feat(backend-el): add cron job for hourly data sync of recreation project comments

--- backend-el/src/main/java/ca/bc/gov/nrs/environment/fta/el/FtaRstExporterApplication.java
+++ backend-el/src/main/java/ca/bc/gov/nrs/environment/fta/el/FtaRstExporterApplication.java
@@ -1,5 +1,8 @@
 package ca.bc.gov.nrs.environment.fta.el;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +14,15 @@ import ca.bc.gov.nrs.environment.fta.el.services.ApplicationService;
 @EnableRetry
 public class FtaRstExporterApplication implements CommandLineRunner {
 
+  private static final Logger logger = LoggerFactory.getLogger(FtaRstExporterApplication.class);
+
+  private static final String JOB_MODE_HOURLY = "hourly";
+  private static final String JOB_MODE_DAILY = "daily";
+
   private final ApplicationService applicationService;
+
+  @Value("${job.mode}")
+  private String jobMode;
 
   public FtaRstExporterApplication(ApplicationService applicationService) {
     this.applicationService = applicationService;
@@ -23,7 +34,14 @@ public class FtaRstExporterApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    this.applicationService.extractAndUploadCSVToS3();
+    if (jobMode.equals(JOB_MODE_HOURLY)) {
+      logger.info("Running hourly CSV extraction and upload");
+      this.applicationService.extractAndUploadCSVToS3Hourly();
+    } else if (jobMode.equals(JOB_MODE_DAILY)) {
+      logger.info("Running daily CSV extraction and upload");
+      this.applicationService.extractAndUploadCSVToS3();
+    } else {
+        logger.info("Invalid job mode specified: {}. Expected {} or {}. Exiting...", jobMode, JOB_MODE_HOURLY, JOB_MODE_DAILY);
+    }
   }
-
 }
