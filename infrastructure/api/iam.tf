9b2a69c81726e166d653c8062d2d2abcb594c2c8
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-06-04 09:02:56 -0700

    chore(backend-el): grant ecs permissions to upload service account

--- infrastructure/api/iam.tf
+++ infrastructure/api/iam.tf
@@ -146,7 +146,12 @@ resource "aws_iam_user_policy" "s3_upload_policy" {
           "s3:ListBucket",
           "s3:GetObject",
           "s3:ListObjects",
-          "s3:PutObject"
+          "s3:PutObject",
+          "iam:PassRole",
+          "ecs:RunTask",
+          "ecs:DescribeTasks",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups"
         ]
         Resource = [
           "arn:aws:s3:::${var.fta_dataload_bucket}",
