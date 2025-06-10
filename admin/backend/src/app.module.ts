7adcdb90b32b489543caaefbe0759e4d13025198
Felipe Barreta <fbarreta@gmail.com>
2025-06-10 15:43:25 -0700

    Poc/823 implement prisma on admin (#892)
    * chore: create schema
    
    * chore: schema generator poc
    
    * chore: add prisma to admin
    
    * chore: remove script generator
    
    * chore: fix prisma files
    
    * chore: schema gen on build
    
    * chore: fix tests
    
    * chore: fix migration
    
    * chore: update .env example

--- admin/backend/src/app.module.ts
+++ admin/backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PassportModule } from "@nestjs/passport";
 import { AuthModule } from "./auth";
+import { PrismaService } from "./prisma.service";
 
 @Module({
   imports: [
@@ -12,6 +13,6 @@ import { AuthModule } from "./auth";
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
