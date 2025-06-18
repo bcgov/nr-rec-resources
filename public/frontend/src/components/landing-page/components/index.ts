671fcc6fc747cde96c0ef52b63aa1c32262fa96e
marcellmueller <marcel@button.is>
2025-06-18 15:14:39 -0400

    feat: add landing page camping info banner

--- public/frontend/src/components/landing-page/components/index.ts
+++ public/frontend/src/components/landing-page/components/index.ts
@@ -1,3 +1,4 @@
+export * from './content-section';
+export { default as InfoBanner } from './info-banner/InfoBanner';
 export * from './section-heading';
 export * from './recreation-search-banner';
-export * from './content-section';
