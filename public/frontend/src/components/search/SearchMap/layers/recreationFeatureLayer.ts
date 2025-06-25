31709522834eac3793dd9db648ad5d2fbc4371d1
marcellmueller <marcel@button.is>
2025-06-25 08:33:21 -0700

    feat: add search fields to map

--- public/frontend/src/components/search/SearchMap/layers/recreationFeatureLayer.ts
+++ public/frontend/src/components/search/SearchMap/layers/recreationFeatureLayer.ts
@@ -33,6 +33,7 @@ export const getCapitalizedName = (name: string): string => {
 
 export const createRecreationIconStyle = (filteredIds: string[] = []) => {
   const filteredSet = new Set(filteredIds);
+  console.log('filteredIds', filteredIds);
 
   return (feature: FeatureLike): Style | undefined => {
     const id = feature.get('FOREST_FILE_ID');
