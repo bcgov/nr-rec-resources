7c528e1d92cd9109223794d9612119dd694f84b8
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-06-26 09:39:49 -0700

    feat(admin-global-notification): added react bootstrap alert notifications

--- admin/frontend/src/App.tsx
+++ admin/frontend/src/App.tsx
@@ -7,15 +7,56 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { LandingPage } from "@/pages/LandingPage";
 import { ROUTES } from "./routes";
+import { useEffect } from "react";
+import { ResponseError } from "./services/recreation-resource-admin";
+import { showNotification } from "./store/notificationStore";
+import { NotificationBar } from "./components/NotificationBar";
 import { RecResourceFilesPage } from "@/components/rec-resource-files/RecResourceFilesPage";
 
+function useGlobalQueryErrorHandler(queryClient: QueryClient) {
+  useEffect(() => {
+    const unsubscribe = queryClient.getQueryCache().subscribe((event) => {
+      if (
+        event.query.state.status === "error" &&
+        event.query.state.fetchStatus === "idle" &&
+        event.query.state.error instanceof ResponseError
+      ) {
+        const status = event.query.state.error.response.status;
+        const toastId = `${status}-error-${JSON.stringify(event.query.queryKey)}`;
+        if (status === 401) {
+          console.log(
+            "Unauthorized access detected. Redirecting to login.",
+            toastId,
+          );
+          showNotification(
+            "401 -  Unauthorized access. Please log in again or contact support.",
+            "danger",
+            toastId,
+          );
+        } else if (status >= 500 && status < 600) {
+          showNotification(
+            `A server error occurred (${status}). Please try again later or contact support.`,
+            "danger",
+            toastId,
+          );
+        }
+      }
+    });
+    return () => unsubscribe();
+  }, [queryClient]);
+}
+
+const queryClient = new QueryClient();
+
 function App() {
-  const queryClient = new QueryClient();
+  useGlobalQueryErrorHandler(queryClient);
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <AuthGuard>
           <Header />
+          <NotificationBar />
           <BrowserRouter>
             <Routes>
               <Route path={ROUTES.LANDING} element={<LandingPage />} />
