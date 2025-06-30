cd6cebc270ec0573c2d4d92a6f62ddd432a69efd
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-06-30 15:19:57 -0700

    feat: added card dropdown

--- admin/frontend/src/components/rec-resource-files/GalleryCard.tsx
+++ admin/frontend/src/components/rec-resource-files/GalleryCard.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEye,
+  faCloudDownload,
+  faTrash,
+  faEllipsisH,
+} from '@fortawesome/free-solid-svg-icons';
 // import './Gallery.scss';
 
 interface GalleryCardProps {
@@ -15,7 +23,29 @@ export const GalleryCard: React.FC<GalleryCardProps> = ({
   topContent,
   filename,
   date,
-  menu = <span className="gallery-card-menu">•••</span>,
+  menu = (
+    <Dropdown align="end">
+      <Dropdown.Toggle
+        variant="link"
+        className="gallery-card-menu p-0 border-0 shadow-none"
+        style={{ color: 'inherit' }}
+        as="span"
+      >
+        <FontAwesomeIcon icon={faEllipsisH} />
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item eventKey="view">
+          <FontAwesomeIcon icon={faEye} className="me-2" /> View
+        </Dropdown.Item>
+        <Dropdown.Item eventKey="download">
+          <FontAwesomeIcon icon={faCloudDownload} className="me-2" /> Download
+        </Dropdown.Item>
+        <Dropdown.Item eventKey="delete">
+          <FontAwesomeIcon icon={faTrash} className="me-2 text-danger" /> Delete
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  ),
 }) => (
   <Card className="gallery-card p-0 h-100">
     <Card.Body
