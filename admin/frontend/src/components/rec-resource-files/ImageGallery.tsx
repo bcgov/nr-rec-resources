60b3a346778a91ac8443b69848b33fec04249317
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-06-30 09:30:02 -0700

    feat: common gallery card component

--- admin/frontend/src/components/rec-resource-files/ImageGallery.tsx
+++ admin/frontend/src/components/rec-resource-files/ImageGallery.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GalleryAccordion } from './GalleryAccordion';
+import { GalleryCard } from './GalleryCard';
 
 export const ImageGallery = ({ images }: { images: any[] }) => (
   <GalleryAccordion
@@ -9,28 +10,18 @@ export const ImageGallery = ({ images }: { images: any[] }) => (
     items={images}
     uploadLabel={'Upload'}
     renderItem={(img, idx) => (
-      <div
+      <GalleryCard
         key={idx}
-        style={{
-          width: 200,
-          borderRadius: '10px',
-          overflow: 'hidden',
-          background: '#f5f5f5',
-          boxShadow: '0 1px 2px #e0e0e0',
-        }}
-      >
-        <img
-          src={img.url}
-          alt={img.name}
-          style={{ width: '100%', height: 100, objectFit: 'cover' }}
-        />
-        <div style={{ padding: '0.5rem 0.75rem' }}>
-          <div style={{ fontWeight: 500, fontSize: 15 }}>{img.name}</div>
-          <div style={{ fontSize: '0.92rem', color: '#757575' }}>
-            {img.date}
-          </div>
-        </div>
-      </div>
+        topContent={
+          <img
+            src={img.url}
+            alt={img.name}
+            style={{ width: '100%', height: 100, objectFit: 'cover' }}
+          />
+        }
+        filename={img.name}
+        date={img.date}
+      />
     )}
   />
 );
