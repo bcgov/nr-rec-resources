43ea3c080e29f8723a56c469e4f1bb947e4f53cf
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-06-30 12:50:40 -0700

    feat: styled info banners in accordian body

--- admin/frontend/src/components/rec-resource-files/ImageGallery.tsx
+++ admin/frontend/src/components/rec-resource-files/ImageGallery.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { GalleryAccordion } from './GalleryAccordion';
 import { GalleryCard } from './GalleryCard';
+import { Image } from 'react-bootstrap';
 
 export const ImageGallery = ({ images }: { images: any[] }) => (
   <GalleryAccordion
     eventKey="images"
-    title="Images"
+    title="Public images"
     description="Image formats JPG, HEIC at max file size 10mb. Maximum 10 images."
     items={images}
     uploadLabel={'Upload'}
     renderItem={(img, idx) => (
       <GalleryCard
         key={idx}
-        topContent={<img src={img.url} alt={img.name} />}
+        topContent={
+          <Image src={img.url} alt={img.name} width={'100%'} height={'100%'} />
+        }
         filename={img.name}
         date={img.date}
       />
