7788d52ddab1d842c467dd4870c1f992365434ff
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-07-02 09:26:05 -0700

    fix(admin-rec-suggestion): modified typeahead filter

--- admin/backend/src/prisma-generated-sql/index.d.ts
+++ admin/backend/src/prisma-generated-sql/index.d.ts
@@ -1,1 +1,3 @@
+export * from "./getRecreationResourceSpatialFeatureGeometry";
+
 export * from "./getRecreationResourceSuggestions";
--- admin/frontend/src/components/rec-resource-files/InfoBanner.tsx
+++ admin/frontend/src/components/rec-resource-files/InfoBanner.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Alert, Stack } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { Alert, Stack } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 export const InfoBanner = ({ children }: { children: React.ReactNode }) => (
   <Alert variant="warning" className="info-banner">
