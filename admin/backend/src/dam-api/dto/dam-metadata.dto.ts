68415814cb224f438b281871ce0e768e3cbdf10c
Felipe Barreta <fbarreta@gmail.com>
2025-08-07 10:33:35 -0700

    chore: add metadata to dam

---
+++ admin/backend/src/dam-api/dto/dam-metadata.dto.ts
@@ -0,0 +1,7 @@
+export class DamMetadataDto {
+  title?: string | null;
+  caption?: string | null;
+  closestCommunity?: string | null;
+  recreationName?: string | null;
+  recreationdistrict?: string | null;
+}
