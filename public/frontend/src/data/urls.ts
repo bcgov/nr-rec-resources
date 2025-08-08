a6208382cec381c938a10a762387dd5cde6b7eaa
Marcel Mueller <marcel@button.is>
2025-08-08 10:29:23 -0700

    chore: update landing page map info (#1139)
    * chore: landing page updates
    
    * chore: update landing page content styling
    
    * test: update e2e landing page assertion
    
    * chore: fix rebase issue

--- public/frontend/src/data/urls.ts
+++ public/frontend/src/data/urls.ts
@@ -3,7 +3,7 @@ export const EXTERNAL_LINKS = {
     'https://helpshapebc.gov.bc.ca/recreation-sites-and-trails-bc/surveys/share-feedback',
   ORIGINAL_SITE: 'https://www.sitesandtrailsbc.ca/',
   RST_GOV_BC_INFO:
-    'https://www2.gov.bc.ca/gov/content/sports-culture/recreation/camping-hiking/sites-trails',
+    'https://www2.gov.bc.ca/gov/content/sports-culture/recreation/camping-hiking/sites-trails/planning/description',
   CONTACT: 'https://www.sitesandtrailsbc.ca/contact-us.aspx',
   FACEBOOK_BC_REC: 'https://www.facebook.com/BCRecSitesandTrails',
   CLOSURE: 'https://www.sitesandtrailsbc.ca/closures.aspx',
@@ -22,3 +22,7 @@ export const EXTERNAL_LINKS = {
   RESEARCH_PARTICIPANT_SIGN_UP:
     'https://submit.digital.gov.bc.ca/app/form/submit?f=02144d43-1806-41d9-a147-553c1a3e0cfd',
 } as const;
+
+export const INTERNAL_LINKS = {
+  SEARCH_MAP: '/search?view=map',
+};
