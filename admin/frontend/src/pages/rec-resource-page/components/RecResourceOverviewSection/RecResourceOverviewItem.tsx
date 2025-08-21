44cf2441c8c77184ee1d3a08f209e76b22cd3341
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-08-21 07:56:25 -0700

    feat: added a styled accordion component

---
+++ admin/frontend/src/pages/rec-resource-page/components/RecResourceOverviewSection/RecResourceOverviewItem.tsx
@@ -0,0 +1,36 @@
+import { Col, ColProps, Stack } from "react-bootstrap";
+
+type RecResourceOverviewItemProps =
+  | {
+      label: string;
+      value: string;
+      isHtml: true;
+      colProps?: ColProps;
+    }
+  | {
+      label: string;
+      value: React.ReactNode;
+      isHtml?: false;
+      colProps?: ColProps;
+    };
+
+export const RecResourceOverviewItem = ({
+  label,
+  value,
+  colProps = { xs: 12, md: 6, lg: 4 },
+  isHtml,
+}: RecResourceOverviewItemProps) => {
+  if (!value) return null;
+  return (
+    <Col {...colProps} className="mb-3">
+      <Stack gap={1}>
+        <span className="fw-bold text-dark small">{label}</span>
+        {isHtml ? (
+          <span dangerouslySetInnerHTML={{ __html: value }} />
+        ) : (
+          <span>{value}</span>
+        )}
+      </Stack>
+    </Col>
+  );
+};
