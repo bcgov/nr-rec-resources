816949c672e7f6c3a6af632d5359e4c03b7a1221
Felipe Barreta <fbarreta@gmail.com>
2025-08-22 10:10:17 -0700

    chore: fix miniature type on search card results (#1181)

--- public/frontend/src/components/rec-resource/card/constants.ts
+++ public/frontend/src/components/rec-resource/card/constants.ts
@@ -1,6 +1,6 @@
 import { RecreationResourceImageVariantDtoSizeCodeEnum } from '@/service/recreation-resource';
 
 export const IMAGE_SIZE_CODE_FOR_SEARCH_RESULTS_CARD: RecreationResourceImageVariantDtoSizeCodeEnum =
-  'hpr';
+  'pre';
 
 export const MAX_ACTIVITIES_TO_DISPLAY = 4;
