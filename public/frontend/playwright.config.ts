9726d12d424fd19ac59b4cc653238db239f9555e
marcellmueller <marcel@button.is>
2025-08-25 14:05:44 -0700

    fix: shared utils env var

--- public/frontend/playwright.config.ts
+++ public/frontend/playwright.config.ts
@@ -1,18 +1,75 @@
-import baseConfig from '@shared/config/playwright.base.config';
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import { BASE_URL } from './e2e/constants';
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
 export default defineConfig({
-  ...baseConfig,
-  use: {
-    ...baseConfig.use,
-    baseURL: BASE_URL,
+  timeout: 120000,
+  expect: {
+    timeout: 10000,
   },
-  projects: (baseConfig.projects ?? []).map((project) => ({
-    ...project,
-    use: {
-      ...project.use,
-      baseURL: BASE_URL,
+  testDir: './e2e',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['line'],
+    ['list', { printSteps: true }],
+    ['html', { open: 'always' }],
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: BASE_URL,
+
+    /* Bypass CSP needed when running e2e on the docker-compose file due to Caddyfile header security */
+    bypassCSP: true,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */ projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: BASE_URL,
+      },
     },
-  })),
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        baseURL: BASE_URL,
+      },
+    },
+
+    {
+      name: 'safari',
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: BASE_URL,
+      },
+    },
+    {
+      name: 'Microsoft Edge',
+      use: {
+        ...devices['Desktop Edge'],
+        channel: 'msedge',
+        baseURL: BASE_URL,
+      },
+    },
+  ],
 });
