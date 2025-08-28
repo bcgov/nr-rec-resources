c85d571e10cb79cb324f491ac3929db2a2fb6252
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-08-28 11:16:51 -0700

    feat: integrated with nestjs cls to provide request id to every log message

--- admin/backend/src/app.module.ts
+++ admin/backend/src/app.module.ts
@@ -2,16 +2,19 @@ import { RecreationResourceModule } from "@/recreation-resource/recreation-resou
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
 import { TerminusModule } from "@nestjs/terminus";
+import { ClsModule } from "nestjs-cls";
+import { ApiMetricsModule } from "./api-metrics/api-metrics.module";
 import { AppConfigModule } from "./app-config/app-config.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth";
+import { clsConfig } from "./common/cls.config";
 import { HealthController } from "./health.controller";
 import { PrismaService } from "./prisma.service";
-import { ApiMetricsModule } from "./api-metrics/api-metrics.module";
 
 @Module({
   imports: [
+    ClsModule.forRoot(clsConfig),
     AppConfigModule,
     PassportModule,
     AuthModule,
