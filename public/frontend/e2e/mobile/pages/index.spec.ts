c22cc011ec132418da92134842622fcb9d3d353e
marcellmueller <marcel@button.is>
2025-09-02 06:50:05 -0700

    test: add mobile e2e page tests and screenshots

--- public/frontend/e2e/mobile/pages/index.spec.ts
+++ public/frontend/e2e/mobile/pages/index.spec.ts
@@ -1,11 +1,15 @@
-import { test } from '@playwright/test';
+import { devices, test } from '@playwright/test';
 import { initHappo } from '@shared/e2e/utils';
 import { LandingPOM, LayoutPOM, UtilsPOM } from 'e2e/poms';
 
 initHappo();
 
-test.describe('Recreation Sites and Trails landing page', () => {
-  test('Landing page renders correctly', async ({ page }) => {
+test.use({
+  ...devices['iPhone 14'],
+});
+
+test.describe('Recreation Sites and Trails landing page (Mobile)', () => {
+  test('Mobile landing page renders correctly', async ({ page }) => {
     const landing = new LandingPOM(page);
     const layout = new LayoutPOM(page);
     const utils = new UtilsPOM(page);
@@ -20,7 +24,7 @@ test.describe('Recreation Sites and Trails landing page', () => {
 
     await utils.screenshot(
       'Recreation Sites and Trails landing page',
-      'default',
+      'mobile',
     );
   });
 });
