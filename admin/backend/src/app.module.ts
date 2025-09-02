6fda9c4ecab546ff2652ec827aabf11d1d30f640
jimmypalelil <jimmy.palelil@gov.bc.ca>
2025-09-02 11:30:21 -0700

    feat(admin-api-metrics): add interceptor to publish metrics to aws cloudwatch

--- admin/backend/src/app.module.ts
+++ admin/backend/src/app.module.ts
@@ -2,20 +2,25 @@ import { RecreationResourceModule } from "@/recreation-resource/recreation-resou
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
 import { TerminusModule } from "@nestjs/terminus";
+import { ClsModule } from "nestjs-cls";
+import { ApiMetricsModule } from "./api-metrics/api-metrics.module";
 import { AppConfigModule } from "./app-config/app-config.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth";
+import { clsConfig } from "./common/cls.config";
 import { HealthController } from "./health.controller";
 import { PrismaService } from "./prisma.service";
 
 @Module({
   imports: [
+    ClsModule.forRoot(clsConfig),
     AppConfigModule,
     PassportModule,
     AuthModule,
     TerminusModule,
     RecreationResourceModule,
+    ApiMetricsModule,
   ],
   controllers: [AppController, HealthController],
   providers: [AppService, PrismaService],
