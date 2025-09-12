d678b4c031f8a404e959a81e395c360a5e97af45
Marcel Mueller <marcel@button.is>
2025-09-12 13:11:40 -0400

    ci: deploy to aws lza (#1217)
    * ci: update terraform for lza deployment
    
    * ci: uncomment tf circular dependencies
    
    * chore: update env vars
    
    * ci: uncomment deployment workflow test changes

--- infrastructure/migration/network.tf
+++ infrastructure/migration/network.tf
@@ -7,13 +7,11 @@ locals {
     unclass = "UnClass"
   }
   environment = local.env_map[lower(var.target_env) == "test" ? "dev" : lower(var.target_env)]
-  vpc_name           = "${local.environment}_vpc"
-  availability_zones = ["a", "b"]
-  app_subnet_names   = [for az in local.availability_zones : "App_${local.environment}_az${az}_net"]
+  vpc_name           = "${local.environment}"
+  availability_zones = ["A", "B"]
+  app_subnet_names  = [for az in local.availability_zones : "${local.environment}-App-${az}"]
 
-  security_group_name_suffix = "_sg"
-
-  app_security_group_name  = "App${local.security_group_name_suffix}"
+  app_security_group_name  = "App"
 }
 
 data "aws_vpc" "main" {
