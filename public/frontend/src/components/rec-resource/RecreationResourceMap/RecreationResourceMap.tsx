d20d1584c1fd567ce95c6f253df0527f061c9357
Felipe Barreta <fbarreta@gmail.com>
2025-10-08 08:57:31 -0700

    chore: added google maps link (#1298)

--- public/frontend/src/components/rec-resource/RecreationResourceMap/RecreationResourceMap.tsx
+++ public/frontend/src/components/rec-resource/RecreationResourceMap/RecreationResourceMap.tsx
@@ -8,6 +8,9 @@ import { Link } from 'react-router-dom';
 import {
   getLayerStyleForRecResource,
   getMapFeaturesFromRecResource,
+  getSitePointFeatureFromRecResource,
+  webMercatorXToLon,
+  webMercatorYToLat,
 } from '@/components/rec-resource/RecreationResourceMap/helpers';
 import { RecreationResourceDetailModel } from '@/service/custom-models';
 import { trackEvent } from '@/utils/matomo';
@@ -20,6 +23,8 @@ import DownloadIcon from '@/images/icons/download.svg';
 import { IconButton } from '@shared/components/icon-button';
 import { SearchMapFocusModes } from '@/components/search-map/constants';
 import { ROUTE_PATHS } from '@/routes/constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLink, faMap } from '@fortawesome/free-solid-svg-icons';
 
 interface RecreationResourceMapProps {
   recResource: RecreationResourceDetailModel;
@@ -42,6 +47,7 @@ const DOWNLOAD_ICON_CONFIG = {
 const TRACKING_ACTIONS = {
   VIEW_IN_MAIN_MAP: 'View in main map',
   EXPORT_MAP_FILE: 'Export map file',
+  OPEN_GOOGLE_MAPS: 'Open in Google Maps',
 } as const;
 
 /**
@@ -147,6 +153,29 @@ export const RecreationResourceMap = ({
     setIsDownloadModalOpen(true);
   }, [recResourceName, recResource?.rec_resource_id]);
 
+  const handleGoogleMapsLink = useCallback(() => {
+    trackEvent({
+      category: MATOMO_TRACKING_CATEGORY_MAP,
+      action: TRACKING_ACTIONS.OPEN_GOOGLE_MAPS,
+
+      name: `${recResourceName}-${recResource?.rec_resource_id}-${TRACKING_ACTIONS.OPEN_GOOGLE_MAPS}`,
+    });
+    const map = getSitePointFeatureFromRecResource(recResource);
+    if (map) {
+      const geometry = map.getGeometry();
+      const coordinates = geometry?.getCoordinates();
+      if (coordinates) {
+        const lat = webMercatorYToLat(coordinates[1]);
+        const lon = webMercatorXToLon(coordinates[0]);
+        console.log(lat, lon);
+        window.open(
+          `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`,
+          '_blank',
+        );
+      }
+    }
+  }, [recResource, recResourceName]);
+
   if (!mapStyledFeatures.length || !downloadStyledFeatures.length) {
     return null;
   }
@@ -166,17 +195,35 @@ export const RecreationResourceMap = ({
         <Row className="g-md-2 g-2">
           <Col xs={12} md="auto">
             <Link
-              to={mainMapUrl}
-              className="text-white"
-              aria-label={`View ${recResourceName} in main map`}
               onClick={handleViewInMainMapClick}
+              aria-label={`View ${recResourceName} in main map`}
+              to={mainMapUrl}
             >
-              <IconButton>View in main map</IconButton>
+              <IconButton
+                data-testid="view-main-map-button"
+                variant="outline"
+                leftIcon={<FontAwesomeIcon icon={faMap} />}
+              >
+                Full map
+              </IconButton>
             </Link>
           </Col>
 
           <Col xs={12} md="auto">
             <IconButton
+              data-testid="google-maps"
+              variant="outline"
+              onClick={handleGoogleMapsLink}
+              aria-label={`Google maps view for ${recResourceName}`}
+              leftIcon={<FontAwesomeIcon icon={faExternalLink} />}
+            >
+              Google Maps
+            </IconButton>
+          </Col>
+
+          <Col xs={12} md="auto">
+            <IconButton
+              data-testid="download-button"
               variant="outline"
               onClick={handleDownloadClick}
               aria-label={`Export map file for ${recResourceName}`}
