0c9d9dd4a4f921cfdd317d47e46883bf8fc0a30c
marcellmueller <marcel@button.is>
2025-10-09 16:51:03 -0400

    fix: prevent was cleared on list view stopping zoom to extent

--- public/frontend/src/components/search-map/types.ts
+++ public/frontend/src/components/search-map/types.ts
@@ -1,15 +1,7 @@
-import { Options as ClusterOptions } from 'ol/source/Cluster';
-
 export interface AnimatedClusterOptions {
   animationDuration?: number;
   declutter?: boolean;
   updateWhileInteracting?: boolean;
   updateWhileAnimating?: boolean;
   renderBuffer?: number;
-}
-
-export interface ExtendedClusterOptions extends ClusterOptions {
-  /* Zoom threshold for clustering -
-   * clusters will be created only if the zoom level is below this value (distance is set to 0) */
-  clusterZoomThreshold?: number;
 }
