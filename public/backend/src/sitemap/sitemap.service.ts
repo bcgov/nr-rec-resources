d7a1ca02cd0069c8a6cade2159bd64daeba4c244
marcellmueller <marcel@button.is>
2025-10-27 13:54:37 -0400

    refactor: tidy up sitemap.service

--- public/backend/src/sitemap/sitemap.service.ts
+++ public/backend/src/sitemap/sitemap.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/prisma.service';
 
+const STATIC_PAGES = ['/', '/contact', '/search'];
+
 @Injectable()
 export class SitemapService {
   constructor(
@@ -27,29 +29,15 @@ export class SitemapService {
     let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
     xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
-    const staticPages = ['/', '/contact', '/search'];
-    for (const page of staticPages) {
-      xml += '  <url>\n';
-      xml += `    <loc>${baseUrl}${page}</loc>\n`;
-      xml += '  </url>\n';
+    for (const page of STATIC_PAGES) {
+      xml += this.buildUrlEntry(`${baseUrl}${page}`);
     }
 
     for (const resource of resources) {
-      // /resource/{rec_resource_id} page
-      xml += '  <url>\n';
-      xml += `    <loc>${baseUrl}/resource/${this.escapeXml(resource.rec_resource_id)}</loc>\n`;
-      if (resource.updated_at) {
-        xml += `    <lastmod>${this.formatDate(resource.updated_at)}</lastmod>\n`;
-      }
-      xml += '  </url>\n';
-
-      // /resource/{rec_resource_id}/contact page
-      xml += '  <url>\n';
-      xml += `    <loc>${baseUrl}/resource/${this.escapeXml(resource.rec_resource_id)}/contact</loc>\n`;
-      if (resource.updated_at) {
-        xml += `    <lastmod>${this.formatDate(resource.updated_at)}</lastmod>\n`;
-      }
-      xml += '  </url>\n';
+      const id = this.escapeXml(resource.rec_resource_id);
+      const lastmod = resource.updated_at;
+      xml += this.buildUrlEntry(`${baseUrl}/resource/${id}`, lastmod);
+      xml += this.buildUrlEntry(`${baseUrl}/resource/${id}/contact`, lastmod);
     }
 
     xml += '</urlset>';
@@ -57,9 +45,14 @@ export class SitemapService {
     return xml;
   }
 
-  private formatDate(date: Date): string {
-    // Format date as YYYY-MM-DD (ISO 8601)
-    return date.toLocaleDateString('en-CA');
+  private buildUrlEntry(url: string, lastmod?: Date): string {
+    let entry = '  <url>\n';
+    entry += `    <loc>${this.escapeXml(url)}</loc>\n`;
+    if (lastmod) {
+      entry += `    <lastmod>${this.formatDate(lastmod)}</lastmod>\n`;
+    }
+    entry += '  </url>\n';
+    return entry;
   }
 
   private escapeXml(unsafe: string): string {
@@ -69,5 +62,10 @@ export class SitemapService {
       .replace(/>/g, '&gt;')
       .replace(/"/g, '&quot;')
       .replace(/'/g, '&apos;');
+  }
+
+  private formatDate(date: Date): string {
+    // Format date as YYYY-MM-DD (ISO 8601)
+    return date.toISOString().split('T')[0];
   }
 }
