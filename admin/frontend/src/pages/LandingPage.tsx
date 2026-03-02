e2189740db00f0269b4a4262571350c29c17d971
marcellmueller <marcel@button.is>
2026-03-02 14:40:06 -0800

    chore: start export data feature

--- admin/frontend/src/pages/LandingPage.tsx
+++ admin/frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
+import { Link } from '@tanstack/react-router';
 import { RecreationResourceSuggestionForm } from '@/components/rec-resource-suggestion-form/RecreationResourceSuggestionForm';
+import { ROUTE_PATHS } from '@/constants/routes';
 import './LandingPage.scss';
 
 export const LandingPage: FC = () => {
@@ -7,6 +9,11 @@ export const LandingPage: FC = () => {
     <div className="landing-page w-100">
       <div className="search-container">
         <RecreationResourceSuggestionForm />
+        <div className="p-4 pt-0">
+          <Link className="exports-link" to={ROUTE_PATHS.EXPORTS}>
+            Data export
+          </Link>
+        </div>
       </div>
     </div>
   );
