03b07a8252f23ca2bd04f541134d1455a4f1a62d
marcellmueller <marcel@button.is>
2026-03-18 13:34:38 -0700

    chore: update index route

--- admin/frontend/src/components/form/index.ts
+++ admin/frontend/src/components/form/index.ts
@@ -1,5 +1,6 @@
 export { TextField } from './TextField';
 export { SelectField } from './SelectField';
+export { CheckboxDropdownField } from './CheckboxDropdownField';
 export { DateInputField } from './DateInputField';
 export { CurrencyInputField } from './CurrencyInputField';
 export * from './GroupedMultiSelectField';
