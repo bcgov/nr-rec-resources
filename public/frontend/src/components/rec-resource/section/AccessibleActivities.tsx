cb3d8843f5153f8708bdcbf2d3fecc15fea27323
Felipe Barreta <fbarreta@gmail.com>
2026-05-04 12:35:46 -0700

    chore: ui fixes

--- public/frontend/src/components/rec-resource/section/AccessibleActivities.tsx
+++ public/frontend/src/components/rec-resource/section/AccessibleActivities.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from 'react';
-import '@/components/rec-resource/section/ThingsToDo.scss';
+import '@/components/rec-resource/section/AccessibleActivities.scss';
 import { SectionIds, SectionTitles } from '@/components/rec-resource/enum';
 import blueIcon from '@/images/icons/trails/blue.svg';
 import greenIcon from '@/images/icons/trails/green.svg';
@@ -46,7 +46,7 @@ const AccessibleActivities = forwardRef<
         return (
           <>
             <section className="mb-4">
-              <h3>{activity.description}</h3>
+              <p className="session-title">{activity.description}</p>
               <p>{activity.details}</p>
               {activity.recreation_activity_trails && (
                 <div>
@@ -60,9 +60,11 @@ const AccessibleActivities = forwardRef<
                           width={48}
                         />
                       </div>
-                      <div className="col-sm">
-                        <h4>{trail.name}</h4>
-                        <p>{trail.description}</p>
+                      <div className="col-sm d-flex flex-column">
+                        <span className="trail-title">{trail.name}</span>
+                        <span className="trail-description">
+                          {trail.description}
+                        </span>
                       </div>
                     </div>
                   ))}
