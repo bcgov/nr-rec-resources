83a1e17530b68e36cd801a71fc40b9dfe93939e8
Diego <diego@oxd.com>
2026-05-05 09:18:45 -0700

    chore: ORCA-788 Update dropdown icons

--- shared/src/components/suggestion-typeahead/constants.ts
+++ shared/src/components/suggestion-typeahead/constants.ts
@@ -1,19 +1,24 @@
 import SIT_ICON from '@shared/assets/icons/recreation_site.svg';
 import RTE_ICON from '@shared/assets/icons/recreation_trail.svg';
+import RTR_ICON from '@shared/assets/icons/recreation_trail_closed.svg';
 import IF_ICON from '@shared/assets/icons/interpretive_forest.svg';
-import NO_TYPE_ICON from '@shared/assets/icons/no_type.svg';
+import TRB_ICON from '@shared/assets/icons/trail_based_recreation_area.svg';
+import RR_ICON from '@shared/assets/icons/recreation_reserve.svg';
+import TBL_ICON from '@shared/assets/icons/trail_based_recreation_linear.svg';
+import IFT_ICON from '@shared/assets/icons/interpretive_forest_trail.svg';
+import ARCHIVED_ICON from '@shared/assets/icons/archived.svg';
 
 export const RESOURCE_TYPE_ICONS: Record<string, string> = {
   // Site type icons:
-  SIT: SIT_ICON,
-  RR: SIT_ICON,
+  SIT: SIT_ICON, // done
+  RR: RR_ICON, // done
   // Trail type icons
-  RTR: RTE_ICON,
-  RTE: RTE_ICON,
-  TBL: RTE_ICON,
-  TRB: RTE_ICON,
+  RTR: RTR_ICON, // done
+  RTE: RTE_ICON, // done
+  TBL: TBL_ICON, // done
+  TRB: TRB_ICON, // done
   // Interpretive forest type icons:
-  IF: IF_ICON,
-  IFT: IF_ICON,
-  NO_TYPE_ICON: NO_TYPE_ICON,
+  IF: IF_ICON, // done
+  IFT: IFT_ICON, // done
+  NO_TYPE_ICON: ARCHIVED_ICON,
 };
