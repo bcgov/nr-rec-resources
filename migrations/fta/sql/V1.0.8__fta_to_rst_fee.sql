bc7751e41a6ee8f35f0ffc4001e70de12ba6c411
ayeshmcg <ayesha.ayesha@gov.bc.ca>
2026-05-25 11:35:22 -0700

    chore: fixed FTA to RST fee migration

--- migrations/fta/sql/V1.0.8__fta_to_rst_fee.sql
+++ migrations/fta/sql/V1.0.8__fta_to_rst_fee.sql
@@ -17,15 +17,15 @@ insert into rst.recreation_fee (
     created_by
 )
 select distinct on (
-        rf.forest_file_id,
-        rf.recreation_fee_code,
-        rf.monday_ind,
-        rf.tuesday_ind,
-        rf.wednesday_ind,
-        rf.thursday_ind,
-        rf.friday_ind,
-        rf.saturday_ind,
-        rf.sunday_ind
+    rf.forest_file_id,
+    rf.recreation_fee_code,
+    rf.monday_ind,
+    rf.tuesday_ind,
+    rf.wednesday_ind,
+    rf.thursday_ind,
+    rf.friday_ind,
+    rf.saturday_ind,
+    rf.sunday_ind
     )
     rf.forest_file_id as rec_resource_id,
     rf.fee_amount,
@@ -65,37 +65,45 @@ on conflict (
     friday_ind,
     saturday_ind,
     sunday_ind
-)
-where is_deleted = false
-do update
+    )
+    do update
+           set
+               fee_amount = excluded.fee_amount,
+           fee_start_date = excluded.fee_start_date,
+           fee_end_date = excluded.fee_end_date,
+           monday_ind = excluded.monday_ind,
+           tuesday_ind = excluded.tuesday_ind,
+           wednesday_ind = excluded.wednesday_ind,
+           thursday_ind = excluded.thursday_ind,
+           friday_ind = excluded.friday_ind,
+           saturday_ind = excluded.saturday_ind,
+           sunday_ind = excluded.sunday_ind,
+           updated_at = excluded.updated_at,
+           updated_by = excluded.updated_by,
+           is_deleted = false,
+           deleted_at = null,
+           deleted_by = null
+       where rst.recreation_fee.is_deleted = false;  -- leave deleted rows alone
+
+
+-- If a fee row is removed in FTA, soft delete it in RST
+update rst.recreation_fee rst_rf
 set
-    fee_amount = excluded.fee_amount,
-    fee_start_date = excluded.fee_start_date,
-    fee_end_date = excluded.fee_end_date,
-    monday_ind = excluded.monday_ind,
-    tuesday_ind = excluded.tuesday_ind,
-    wednesday_ind = excluded.wednesday_ind,
-    thursday_ind = excluded.thursday_ind,
-    friday_ind = excluded.friday_ind,
-    saturday_ind = excluded.saturday_ind,
-    sunday_ind = excluded.sunday_ind,
-    updated_at = excluded.updated_at,
-    updated_by = excluded.updated_by;
-
-
--- If a fee row is removed in FTA, remove it in RST
-delete from rst.recreation_fee rst_rf
+    is_deleted = true,
+    deleted_at = now(),
+    deleted_by = 'FLYWAY'
 where not exists (
     select 1
     from fta.recreation_fee rf
     where
         rf.forest_file_id = rst_rf.rec_resource_id
-        and rf.recreation_fee_code = rst_rf.recreation_fee_code
-        and rf.monday_ind = rst_rf.monday_ind
-        and rf.tuesday_ind = rst_rf.tuesday_ind
-        and rf.wednesday_ind = rst_rf.wednesday_ind
-        and rf.thursday_ind = rst_rf.thursday_ind
-        and rf.friday_ind = rst_rf.friday_ind
-        and rf.saturday_ind = rst_rf.saturday_ind
-        and rf.sunday_ind = rst_rf.sunday_ind
-);
+      and rf.recreation_fee_code = rst_rf.recreation_fee_code
+      and rf.monday_ind = rst_rf.monday_ind
+      and rf.tuesday_ind = rst_rf.tuesday_ind
+      and rf.wednesday_ind = rst_rf.wednesday_ind
+      and rf.thursday_ind = rst_rf.thursday_ind
+      and rf.friday_ind = rst_rf.friday_ind
+      and rf.saturday_ind = rst_rf.saturday_ind
+      and rf.sunday_ind = rst_rf.sunday_ind
+)
+  and rst_rf.is_deleted = false;
