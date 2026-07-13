0827d08e80324bd3224526423e54705ad515510b
Diego <diego@oxd.com>
2026-07-13 13:18:15 -0700

    chore: Update admin backend schema decorator

--- admin/backend/src/app-config/app-config.schema.ts
+++ admin/backend/src/app-config/app-config.schema.ts
@@ -53,9 +53,9 @@ export class EnvironmentVariables {
   // bearer strategy validates against. Act-issued tokens will be rejected
   // by the (RST admin) global strategy because the audience differs - and
   // vice-versa - which is exactly the isolation we want.
-  @IsOptional()
   @IsString()
-  ACT_CSS_CLIENT_ID?: string;
+  @IsNotEmpty()
+  ACT_CSS_CLIENT_ID: string;
 
   @IsOptional()
   @IsString()
